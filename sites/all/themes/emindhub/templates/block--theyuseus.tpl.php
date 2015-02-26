979e32530aa385bd04672c3a81658a728f19bfec
Administrator <ybabel@free.fr>
2015-02-26 17:02:25 +0100

    Theme : carrousel and bottom blocks

---
+++ sites/all/themes/emindhub/templates/block--theyuseus.tpl.php
@@ -0,0 +1,14 @@
+<?php if ($block->subject): ?>
+    <div class="row paddingUD">
+        <div class="col-md-5 light-blue-text bold">
+            <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+        </div>
+        <div class="col-md-6"><hr class="hr-light"></div>
+        <div class="col-md-1">
+            <img src="<?php print url(imagePath('fluxIcon.png')); ?>">
+        </div>
+    </div>
+<?php endif;?>
+<?php if ($content): ?>
+    <?php print $content; ?>
+<?php endif;?>
