8ed22be50811a725c9d5e4ae3cd972acc14e6131
Administrator <ybabel@free.fr>
2015-03-05 14:26:04 +0100

    Theme : my-circles, my-contributions, register

---
+++ sites/all/themes/emindhub/templates/unused/__block--menu--menu-top-menu.tpl.php
@@ -0,0 +1,7 @@
+toto le haricot
+
+<div>
+    <div><?php render($elements[3779]); ?></div>
+    <div><?php render($elements[3780]); ?></div>
+    <div><?php render($elements[3781]); ?></div>
+</div>
---
+++ sites/all/themes/emindhub/templates/unused/__block--user-register.tpl.php
@@ -0,0 +1,3 @@
+$variables['elements']['#form_id'] = "user_register_form"
+$variables['elements']['#action'] = /business/register
+<?php echo 'toto'; ?>
