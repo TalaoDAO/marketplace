4152e872e1148e8ad470b21db0993dff141b897e
Administrator <ybabel@free.fr>
2015-03-16 22:18:12 +0100

    Theme : little enhancment page my-circle and user_alert

---
+++ sites/all/themes/emindhub/templates/user-alert.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Displays a user alert.
+ *
+ * Available variables:
+ * - $alert_label: The label of the alert, as set in the User Alerts settings.
+ * - $body: The user alert message.
+ *
+ * @ingroup themeable
+ */
+?>
+<div id="user-alert-<?php print $nid; ?>" class="user-alert">
+   <?php if ($is_closeable) : ?><div class="user-alert-close"><a href="javascript:;" rel="<?php print $nid; ?>">x</a></div><?php endif; ?>
+   <div class="user-alert-message user-alert-level<?php print $node->field_alert_level[LANGUAGE_NONE][0]['value']; ?>" ><?php if ($alert_label) : ?><span class="user-label"><?php print $alert_label; ?>:</span><?php endif; ?> <?php print $body; ?></div>
+</div>
