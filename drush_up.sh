fd7590221fecbab58cf663a7ebaa551bc3982e4b
Administrator <ybabel@free.fr>
2015-04-02 12:11:03 +0200

    Update script : add og entity reference patch https://www.drupal.org/node/2249261

--- drush_up.sh
+++ drush_up.sh
@@ -1,3 +1,4 @@
 drush up
 git apply drupal-7.x-Issue-2028713-by-axel.rutz__0.patch
 git apply bartick-menu-images.patch
+git apply entityreference_fields_do_not_validate-2249261-10.patch --directory=sites/all/modules/og/
