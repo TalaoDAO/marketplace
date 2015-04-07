66dc1476fc9f7024fcc03a5d3ce04c8b9b1b2dbc
Administrator <ybabel@free.fr>
2015-04-07 17:19:43 +0200

    [dotsafe] : Deleting RSS Icons on they are expert and they use use blocks Closes #94

--- sites/all/themes/emindhub/templates/block--theyareexpert.tpl.php
+++ sites/all/themes/emindhub/templates/block--theyareexpert.tpl.php
@@ -1,12 +1,12 @@
 <?php if ($block->subject): ?>
     <div class="row paddingUD">
-        <div class="col-md-8 light-blue-text bold">
+        <div class="col-md-8 col-xs-12 light-blue-text bold">
             <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
         </div>
-        <div class="col-md-3"><hr class="hr-light"></div>
-        <div class="col-md-1 noPaddingL">
-            <img src="<?php print getImgSrc('fluxIcon.png'); ?>">
-        </div>
+        <div class="col-md-3 col-xs-6"><hr class="hr-light"></div>
+        <!--<div class="col-md-1 col-xs-1 noPaddingL">
+            <img src="<?php /*print getImgSrc('fluxIcon.png'); */?>">
+        </div>-->
     </div>
 <?php endif;
 if ($content):
