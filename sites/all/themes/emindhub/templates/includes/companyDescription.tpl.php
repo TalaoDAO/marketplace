4af4220a4ee2dcc96f80e02a0cc62c6ebc1df6e0
Administrator <ybabel@free.fr>
2015-04-08 17:35:08 +0200

    [dotsafe] : changing bold Closes #127

--- sites/all/themes/emindhub/templates/includes/companyDescription.tpl.php
+++ sites/all/themes/emindhub/templates/includes/companyDescription.tpl.php
@@ -4,7 +4,7 @@ if (isset($field_anonymous[0]['value']) && $field_anonymous[0]['value'] == 1) { 
         <div class="col-md-3 bold paddingU"><?php ddl(get_defined_vars()); print $elements['field_entreprise_description']['#title']; ?></div>
     </div>
     <div class="row">
-        <div class="col-md-12 bold">
+        <div class="col-md-12">
             <?php
             if (isset($field_use_my_entreprise) && $field_use_my_entreprise[0]['value'] != 0) {
                 print $field_entreprise_description[0]['value'];
