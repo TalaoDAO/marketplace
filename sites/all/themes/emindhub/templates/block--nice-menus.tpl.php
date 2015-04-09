a590c03610c9facd247de247186a618bba83a4f9
Administrator <ybabel@free.fr>
2015-04-09 10:01:47 +0200

    [dotsafe] : adding somme styles for nice menus

--- sites/all/themes/emindhub/templates/block--nice-menus.tpl.php
+++ sites/all/themes/emindhub/templates/block--nice-menus.tpl.php
@@ -1,12 +1,16 @@
-<div class="title-wrapper">
-    <div class="row">
-        <div class="col-md-4">
-            <hr class="hr-dark" />
-        </div>
-        <div  class="col-md-4 title"><?php print $block->title ?></div>
-        <div class="col-md-4">
-            <hr class="hr-dark" />
+<?php if ($block->title) { ?>
+    <div class="title-wrapper">
+        <div class="row">
+            <div class="col-md-4">
+                <hr class="hr-dark" />
+            </div>
+            <div  class="col-md-4 title"><?php print $block->title ?></div>
+            <div class="col-md-4">
+                <hr class="hr-dark" />
+            </div>
         </div>
     </div>
-</div>
-<?php print $content ?>
+<?php } else { ?>
+    <div class="title-wrapper"></div>
+<?php }
+print $content ?>
