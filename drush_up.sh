2d0fc6b524a1ad06d72f4e7e63355f5c9ce6e86b
Administrator <ybabel@free.fr>
2015-05-07 10:30:21 +0200

    Move patches to patchs directory

--- drush_up.sh
+++ drush_up.sh
@@ -1,5 +1,6 @@
 drush up
-git apply drupal-7.x-Issue-2028713-by-axel.rutz__0.patch
-git apply bartick-menu-images.patch
-git apply mb_custom-settings-permission_2264913-1.patch --directory=sites/all/modules/contrib
-git apply entityreference_fields_do_not_validate-2249261-10.patch --directory=sites/all/modules/og/
+git apply patchs/drupal-7.x-Issue-2028713-by-axel.rutz__0.patch
+git apply patchs/bartick-menu-images.patch
+git apply patchs/mb_custom-settings-permission_2264913-1.patch --directory=sites/all/modules/contrib
+git apply patchs/entityreference_fields_do_not_validate-2249261-10.patch --directory=sites/all/modules/og/
+git apply patchs/schema-1850196-autoslave_incompatibility-4.patch --directory=sites/all/modules/debug/schema
