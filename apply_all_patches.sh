680059968fae27f70b81ae072225f1b528e0d3c6
Administrator <ybabel@free.fr>
2015-06-05 15:49:31 +0200

    Field collection remove button patch, closes #148

--- apply_all_patches.sh
+++ apply_all_patches.sh
@@ -4,3 +4,4 @@ git apply patchs/bartick-menu-images.patch
 git apply patchs/mb_custom-settings-permission_2264913-1.patch --directory=sites/all/modules/contrib
 git apply patchs/entityreference_fields_do_not_validate-2249261-10.patch --directory=sites/all/modules/og/
 git apply patchs/schema-1850196-autoslave_incompatibility-4.patch --directory=sites/all/modules/debug/schema
+git apply patchs/field_collection-remove_button_broken-1675522-12.patch --directory=sites/all/modules/contrib/field_collection
