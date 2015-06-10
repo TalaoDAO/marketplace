85fd565b7e39e70a7f0d32f96aa8504e721038da
Administrator <ybabel@free.fr>
2015-06-10 18:24:12 +0200

    dotsafe : correct scrolling problem on "our-services" page due to fixe toolbar on top of the site

--- sites/all/themes/emindhub/js/main.js
+++ sites/all/themes/emindhub/js/main.js
@@ -91,6 +91,11 @@ function onClickBodySignUp(evt) {
     document.body.onclick = null;
 }
 
+
+var shiftWindow = function() { scrollBy(0, -150) };
+window.addEventListener("hashchange", shiftWindow);
+function load() { if (window.location.hash) shiftWindow(); }
+
 /*
 function signUpHover(evt) {
 
