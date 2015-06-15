c3e6c7db11149a5e32e24f1a972ffeccf62992ef
delaBruyne <pro@laetitiadebruyne.com>
2015-06-15 12:08:03 +0200

    FIX #165 (re)show lastname

--- sites/all/themes/emindhub/templates/views/views-view-fields--users-list--block-2.tpl.php
+++ sites/all/themes/emindhub/templates/views/views-view-fields--users-list--block-2.tpl.php
@@ -1,25 +1,26 @@
 <div class="row paddingU">
 
-        <?php
-        if ($fields['field_photo']) { ?>
-        <div class="col-xs-6">
-            <?php
-            if ($fields['field_photo']) {
-                print $fields['field_photo']->content;
-            }
-            ?>
-        </div>
-        <div class="col-xs-6">
-            <?php }
-            else { ?>
-            <div class="col-md-12">
-                <?php } ?>
-                <div class="light-blue-text">
-                    <?php print $fields['field_first_name']->content; ?>
-                </div>
-                <div class="light-blue-text bold">
-                    <?php //print $fields['field_last_name']->content; ?>
-                </div>
-                <?php print $fields['field_titre_metier']->content; ?>
-            </div>
-        </div>
+		<?php if ($fields['field_photo']) { ?>
+		<div class="col-xs-6">
+			<?php
+			if ($fields['field_photo']) {
+				print $fields['field_photo']->content;
+			}
+			?>
+		</div>
+		<div class="col-xs-6">
+
+		<?php } else { ?>
+		<div class="col-md-12">
+		<?php } ?>
+
+  		<div class="light-blue-text">
+  			<?php print $fields['field_first_name']->content; ?>
+  		</div>
+  		<div class="light-blue-text bold">
+  			<?php print $fields['field_last_name']->content; ?>
+  		</div>
+  		<?php print $fields['field_titre_metier']->content; ?>
+	</div>
+
+</div>
