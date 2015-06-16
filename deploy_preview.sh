f528d8c6a73070ccc083b27d8bd1609356a85c53
Administrator <ybabel@free.fr>
2015-06-16 13:06:52 +0200

    Small git & deploy fix

--- deploy_preview.sh
+++ deploy_preview.sh
@@ -18,9 +18,8 @@ echo 'flush_all' | nc -q 2 localhost 11211
 ############################### PREVIEW #################################
 cd /var/www/preview-emindhub
 drush dis -y devel, get_form_id, diff, devel_debug_log, pathinfo, views_maintenance, admin_devel, dbtng_migrator, hacked, masquerade, memcache_admin, module_filter, security_review, seo_checklist, stickynote, switchtheme, unused_modules, robotstxt
-drush en -y entity_legal, user_alert
-drush dis -y admin_menu
-drush dis -y reroute_email, userpoints
+drush en -y entity_legal, user_alert, statistics
+drush dis -y admin_menu, reroute_email, userpoints
 drush vset -y error_level 0
 drush vset drush vset user_registrationpassword_registration with-pass ; drush vset user_mail_register_pending_approval_notify FALSE ; drush vset user_mail_register_no_approval_required_notify FALSE 
 drush sqlq "delete from autoassignrole_page where rid_page_id=2;"
