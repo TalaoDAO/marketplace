ca6422ef029ce6416b4463dd69ff9e5c2f559005
root <yoann.babel@emindhub.com>
2015-07-04 13:11:42 +0200

    deploy refatoring to put it all in git

--- deploy_theme_preview.sh
+++
@@ -1,5 +0,0 @@
-set -x
-rm -r /var/www/preview-emindhub/sites/all/themes/emindhub
-cp -r /var/www/dev-emindhub/sites/all/themes/emindhub /var/www/preview-emindhub/sites/all/themes/
-drush @preview cc theme-registry
-drush @preview cc css-js
