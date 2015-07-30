2ece41a32c1bb653ce5fd163ec240fc0c155cd97
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-07-30 12:28:47 +0200

    MAJ tests CasperJS + PhantomCSS

--- tests_casperjs/csstest.js
+++ tests_casperjs/csstest.js
@@ -56,6 +56,7 @@ var anonymousURLs = [
 var profiles = [
   {
     'name': 'Anonymous',
+    // 'login': {  },
     'urls': [
       config.host,                        // Homepage
       config.host + 'our-services',       // Our services
@@ -68,21 +69,25 @@ var profiles = [
     'name': 'Expert',
     'login': { 'name': 'expert1', 'pass': 'expert1' },
     'urls': [
-      config.host,
-      config.host + 'user/2/drafts',
-      config.host + 'content/my-responses-0',
+      config.host,                            // Homepage
+      config.host + 'user/2/drafts',          // My drafts
+      config.host + 'content/my-responses-0', // My responses
       config.host + 'user',
       config.host + 'user/2/edit',
       config.host + 'messages',
       config.host + 'my-selections',
     ],
+    // 'areas': [
+    //   'name': 'Burger menu',
+    //   'selector':
+    // ],
   },
   {
     'name': 'Business',
     'login': { 'name': 'business1', 'pass': 'business1' },
     'urls': [
-      config.host,
-      config.host + 'user/4/drafts',
+      config.host,                        // Homepage
+      config.host + 'user/4/drafts',      // My drafts
       config.host + 'content/my-responses-0',
       config.host + 'user',
       config.host + 'user/4/edit',
@@ -95,21 +100,13 @@ var profiles = [
   }
 ];
 
-// var nodeContents = {
-//   'title': 'Hello, World!',
-//   'body[und][0][value]': 'This content was added by CasperJS!'
-// };
-
-// TODO
-// var screenshotName = profiles.profile.name + '_' + this.getTitle() + '_' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height;
-
 phantomcss.init( {
   rebase: casper.cli.get( "rebase" ),
   casper: casper,
   libraryRoot: fs.absolute( fs.workingDirectory + '/node_modules/phantomcss' ),
   screenshotRoot: fs.absolute( fs.workingDirectory + '/screenshots' ),
   failedComparisonsRoot: fs.absolute( fs.workingDirectory + '/failures' ),
-  comparisonResultRoot: '/results',
+  comparisonResultRoot: fs.absolute( fs.workingDirectory + '/results' ),
   addLabelToFailedImage: false,
 } );
 
@@ -120,7 +117,7 @@ phantomcss.init( {
 //     self.thenOpen(link, function() {
 //       console.log('Current location is "' + this.getTitle() + '" (' + this.getCurrentUrl() + ')');
 //       casper.each(viewports, function(casper, viewport) {
-//         this.then(function() {
+//         casper.then(function() {
 //           this.viewport(viewport.viewport.width, viewport.viewport.height);
 //           console.log('Screenshot for ' + viewport.name + ' (' + viewport.viewport.width + 'x' + viewport.viewport.height + ')');
 //           phantomcss.screenshot( 'body', profiles.profile.name + '_' + this.getTitle() + '_' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height );
@@ -135,7 +132,7 @@ phantomcss.init( {
 //
 //   casper.run(function () {
 //     console.log( '\nTHE END.' );
-//     this.test.done();
+//     casper.test.done();
 //   });
 //
 // });
@@ -143,39 +140,114 @@ phantomcss.init( {
 
 casper.test.begin('Testing roles environnement', function suite(test) {
 
-  casper.start( config.host );
+  casper.start( config.host, function() {
 
-  casper.then().each(profiles, function() {
+    casper.each(profiles, function( casper, profile ) {
 
-    if (profiles.profile.name !== 'anonymous') {
-      // casper.then(function() {
-        casper.fill('form#user-login-form', profiles.profile.login.name, true);
-        this.test.comment('Logging in...');
-      // });
+      casper.then( function() {
 
-      // casper.then(function() {
-        this.test.assertHttpStatus(200, "Authentication successful");
-        this.test.assertExists('body.logged-in', 'Drupal class for logged-in users was found.');
-        phantomcss.screenshot('body', 'Expert Homepage Body');
-      // });
-    }
+        casper.test.comment( 'The current profile is ' + profile.name );
 
-    this.then().each(urls, function(self, link) {
-      self.thenOpen(link, function() {
-        console.log('Current location is "' + this.getTitle() + '" (' + this.getCurrentUrl() + ')');
-        casper.each(viewports, function(casper, viewport) {
-          this.then(function() {
-            this.viewport(viewport.viewport.width, viewport.viewport.height);
-            console.log('Screenshot for ' + viewport.name + ' (' + viewport.viewport.width + 'x' + viewport.viewport.height + ')');
-            phantomcss.screenshot( 'body', profiles.profile.name + '_' + this.getTitle() + '_' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height );
+        casper.then(function() {
+          casper.thenOpen(config.host + 'user/logout', function() {
+            casper.test.comment( 'Logging out... just in case' );
           });
         });
+
+        if ( profile.name === 'Anonymous' ) {
+
+          casper.then(function() {
+            casper.test.assertExists('body.not-logged-in', 'Drupal class for not logged-in users was found.');
+          });
+
+        } else {
+
+          casper.thenOpen(config.host, function() {
+            casper.test.comment( 'Going back to homepage... just in case' );
+            casper.fill('form#user-login-form', profile.login, true);
+            casper.test.comment( 'Logging in...' );
+          });
+
+          casper.then(function() {
+            casper.test.assertHttpStatus(200, "Authentication successful");
+            casper.test.assertExists('body.logged-in', 'Drupal class for logged-in users was found.');
+          });
+
+          // Burger menu
+          casper.then( function () {
+            casper.click( '.burger-menu-btn-container' );
+
+            // wait for modal to fade-in
+            casper.waitForSelector( '.region.region-burgermenu:not([style*="display: none"])',
+              function success() {
+                casper.test.comment( 'Hello burger !' );
+
+                casper.each(viewports, function(casper, viewport) {
+
+                  casper.then(function() {
+
+                    this.viewport(viewport.viewport.width, viewport.viewport.height);
+                    casper.test.comment( 'Taking a screenshot for ' + viewport.name + ' (' + viewport.viewport.width + 'x' + viewport.viewport.height + ')' );
+                    phantomcss.screenshot( '.region.region-burgermenu', profile.name + '_Burger-menu_' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height );
+
+                  });
+
+                }); // END each viewports
+
+              },
+              function timeout() {
+                casper.test.fail( 'Should see burger menu' );
+              }
+            );
+          } );
+
+          casper.then( function () {
+            casper.click( '.burgermenu-close-icon' );
+
+            // wait for modal to fade-out
+            casper.waitForSelector( '.region.region-burgermenu[style*="display: none"]',
+              function success() {
+                casper.test.comment( 'Bye bye burger !' );
+              },
+              function timeout() {
+                casper.test.fail( 'Should be able to walk away from the burger menu' );
+              }
+            );
+          } );
+
+        }
+
+        casper.then(function() {
+
+          casper.each(profile.urls, function(self, link) {
+
+            self.thenOpen(link, function() {
+
+              casper.test.comment( 'This is the location "' + this.getTitle() + '" (' + this.getCurrentUrl() + ')' );
+
+              casper.each(viewports, function(casper, viewport) {
+
+                casper.then(function() {
+
+                  this.viewport(viewport.viewport.width, viewport.viewport.height);
+                  casper.test.comment( 'Taking a screenshot for ' + viewport.name + ' (' + viewport.viewport.width + 'x' + viewport.viewport.height + ')' );
+                  phantomcss.screenshot( 'body', profile.name + '_' + this.getTitle() + '_' + viewport.name + '-' + viewport.viewport.width + 'x' + viewport.viewport.height );
+
+                });
+
+              }); // END each viewports
+
+            });
+
+          }); // END each profile urls
+
+        });
+
       });
-    });
 
-    this.thenOpen(config.host + 'user/logout', function() {  });
+    }); // END each profiles
 
-  });
+  }); // END start
 
   casper.then( function now_check_the_screenshots() {
     phantomcss.compareAll();
@@ -183,44 +255,8 @@ casper.test.begin('Testing roles environnement', function suite(test) {
 
   casper.run(function () {
     console.log( '\nTHE END.' );
-    this.test.done();
+    casper.test.done();
   });
-
-  // casper.start(config.host, function() {
-  //   casper.fill('form#user-login-form', profiles.profile.expert.login, true);
-  //   this.test.comment('Logging in...');
-  // });
-  //
-  // casper.then(function() {
-  //   this.test.assertHttpStatus(200, "Authentication successful");
-  //   this.test.assertExists('body.logged-in', 'Drupal class for logged-in users was found.');
-  //   phantomcss.screenshot('body', 'Expert Homepage Body');
-  // });
-
-  // casper.thenOpen(config.host + 'admin/help', function() {
-  //   phantomcss.screenshot('body', 'Expert Homepage Body');
-  // });
-
-  // casper.thenOpen(config.host + 'admin/help').waitForText('Help',
-  //   function then() {
-  //     this.test.assertTextExists('Help topics', 'Help topics');
-  //     this.test.assertDoesntExist('#user-login', 'Login form not present on handbook page');
-  //   },
-  //   function timeout() {
-  //     this.test.assert(false, 'Loaded Online Help for administrator.');
-  //   }
-  // );
-
-  // casper.thenOpen(config.host + 'user/logout', function() {  });
-  //
-  // casper.then( function now_check_the_screenshots() {
-  //   phantomcss.compareAll();
-  // } );
-  //
-  // casper.run(function () {
-  //   console.log( '\nTHE END.' );
-  //   this.test.done();
-  // });
 
 });
 
