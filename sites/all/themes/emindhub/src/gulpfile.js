d2fc80191f34477ad35a1d51e8150829d80b55ca
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-08-27 21:54:54 +0200

    MAJ gulp process : debug

--- sites/all/themes/emindhub/src/gulpfile.js
+++ sites/all/themes/emindhub/src/gulpfile.js
@@ -8,7 +8,7 @@ var sourcemaps   = require('gulp-sourcemaps');
 // var clean        = require('gulp-clean');
 // var concat       = require('gulp-concat');
 // var cache        = require('gulp-cache');
-// var debug        = require('gulp-debug');
+var debug        = require('gulp-debug');
 // var path         = require('path');
 
 
@@ -16,11 +16,13 @@ var theme_path  = "../";
 var watch_files = [theme_path + 'src/less/**/*.less'];
 var src_less    = [theme_path + 'src/less/style.less'];
 
+
 w = process.cwd();
 
 gulp.task('less', function () {
   gulp.src(src_less)
     .pipe(sourcemaps.init())
+    .pipe(debug({title: 'unicorn:'}))
     .pipe(less())
     .pipe(rename("style.css"))
     .pipe(sourcemaps.write('.', { sourceRoot: '../src/less' }))
