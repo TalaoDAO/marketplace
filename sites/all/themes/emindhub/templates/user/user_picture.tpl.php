565bfa4faa2f497f8cda30344ff9d29c1280dd5d
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-08-27 15:48:10 +0200

    MAJ code alinea

--- sites/all/themes/emindhub/templates/user/user_picture.tpl.php
+++ sites/all/themes/emindhub/templates/user/user_picture.tpl.php
@@ -1,14 +1,15 @@
 <?php
 $account = user_load($variables['account']->uid);
+?>
 
-if ($account) {
-    if ($account->field_photo && $account->field_photo[LANGUAGE_NONE][0]) {
-        $path = $account->field_photo[LANGUAGE_NONE][0]['uri'];
-    }
-}
+<?php if ($account) {
+  if ($account->field_photo && $account->field_photo[LANGUAGE_NONE][0]) {
+    $path = $account->field_photo[LANGUAGE_NONE][0]['uri'];
+  }
+} ?>
 
-if(isset($path) && $path) {   ?>
-    <div class="profile-picture">
-        <img src="<?php print image_style_url('profile_picture', $path); ?>"/>
-    </div>
+<?php if(isset($path) && $path) { ?>
+<div class="profile-picture">
+  <img src="<?php print image_style_url('profile_picture', $path); ?>" />
+</div>
 <?php } ?>
