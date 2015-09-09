5dc8b0261939f01b8829d97279fee8fd197657d1
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-09-09 12:30:03 +0200

    DEL tpl for Benefits block + ADD model tpl for sidebar

--- sites/all/themes/emindhub/templates/block/block--sidebar_second.tpl.php
+++ sites/all/themes/emindhub/templates/block/block--sidebar_second.tpl.php
@@ -49,18 +49,14 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2<?php print $title_attributes; ?>><span><?php print $title; ?></span></h2>
+    <h3<?php print $title_attributes; ?>><span><?php print $title; ?></span></h3>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
   <?php if ($content): ?>
     <div class="content">
-      <?php print $content ?>
+      <?php print $content; ?>
     </div>
   <?php endif;?>
-
-  <div class="block-footer">
-    <?php print c_szClickToZoom; ?>
-  </div>
 
 </section> <!-- /.block -->
