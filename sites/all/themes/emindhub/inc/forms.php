cb373ef804f094a36b3d7b494aab64a4c6a4cf43
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-10-22 15:19:24 +0200

    OUPS

--- sites/all/themes/emindhub/inc/forms.php
+++ sites/all/themes/emindhub/inc/forms.php
@@ -21,7 +21,7 @@ function emindhub_process_format($element) {
 
 function emindhub_form_alter(&$form, &$form_state, $form_id) {
 
-  echo '<pre>' . print_r($form_id, TRUE) . '</pre>';
+  // echo '<pre>' . print_r($form_id, TRUE) . '</pre>';
   // echo '<pre>' . print_r(element_children($form), TRUE) . '</pre>';
 
   $form['actions']['#prefix'] = '<div class="form-row">';
