118b1ca69712f88c6d727fcc795d81271939531d
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-10-28 15:35:46 +0100

    FIX: hide survey submission when survey is closed #317

--- sites/all/themes/emindhub/templates/node--webform.tpl.php
+++ sites/all/themes/emindhub/templates/node--webform.tpl.php
@@ -202,13 +202,15 @@
 			hide($content['links']);
 			// print render($content);
 		?>
-
+		
+		<?php if ($node->webform['status'] != 0) : ?>
 		<div id="comments" class="<?php print $classes; ?> row section emh-fieldgroup-blue-title"<?php print $attributes; ?>>
 	    <h2 class="h3"><span><?php print t('Answer the survey') ?></span></h2>
 	    <div class="field-group-div">
 	      <?php print render($content['webform']); ?>
 	    </div>
 		</div>
+	<?php endif; ?>
 	</div>
 
 </div>
