2ece30810f2c3eb3bb2e47ab8e9aff5fa756a2a7
Laetitia Debruyne <pro@laetitiadebruyne.com>
2015-11-25 09:34:26 +0100

    CHG: #473 block title style

--- sites/all/themes/emindhub/inc/blocks.php
+++ sites/all/themes/emindhub/inc/blocks.php
@@ -213,6 +213,12 @@ function emindhub_preprocess_block(&$vars) {
     case 'progress_tracker-progress_tracker':
       $classes[] = 'emh-block-blue-title';
       break;
+
+    // Parrainage
+    case 'emh_virality-invitation_form':
+      $classes[] = 'emh-block-blue-title';
+      break;
+      
   }
 
   // echo '<pre>' . print_r($vars, TRUE) . '</pre>';
