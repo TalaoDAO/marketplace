5175d287f41a6cce150709fc3e50e19906a95711
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-01-19 16:10:46 +0100

    CHG: better email template + CHG: dynamic Piwik campaign info tracking

--- sites/all/themes/emindhub/templates/mail/mimemail-message.tpl.php
+++ sites/all/themes/emindhub/templates/mail/mimemail-message.tpl.php
@@ -38,19 +38,19 @@ global $base_url;
     <?php endif; ?>
 		<!--[if mso 12]>
 			<style type="text/css">
-				.flexibleContainer{display:block !important; width:100% !important;}
+				.flexibleContainer{display:block!important;width:100%!important;}
 			</style>
 		<![endif]-->
 		<!--[if mso 14]>
 			<style type="text/css">
-				.flexibleContainer{display:block !important; width:100% !important;}
+				.flexibleContainer{display:block!important;width:100%!important;}
 			</style>
 		<![endif]-->
 	</head>
 	<body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?> bgcolor="#F2F2F2" leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
 
 		<center style="background-color:#F2F2F2;">
-			<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable" style="table-layout: fixed;max-width:100% !important;width: 100% !important;min-width: 100% !important;">
+			<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable" style="table-layout:fixed;max-width:100%!important;width:100%!important;min-width:100%!important;">
 				<tr>
 					<td align="center" valign="top" id="bodyCell">
 
@@ -70,8 +70,8 @@ global $base_url;
 																	<td align="center" valign="middle" class="flexibleContainerBox">
 																		<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:100%;">
 																			<tr>
-																				<td align="center" class="textContent" style="padding: 9px 18px; text-align: center;">
-																					<a href="http://www.emindhub.com/?pk_campaign=website&amp;pk_kwd=logo" target="_blank"><img alt="eMindHub" src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/logo/eMindHub_logo_baseline.png" style="width: 200px; height: 78px; margin: 0px;" height="78" width="200" align="center"></a>
+																				<td align="center" class="textContent" style="padding:9px 18px;text-align:center;">
+																					<a href="<?php print $base_url; ?>/?pk_campaign=website&amp;pk_kwd=logo" target="_blank"><img alt="eMindHub" src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/logo/eMindHub_logo_baseline.png" style="width:200px;height:78px;margin:0px;" height="78" width="200" align="center"></a>
 																				</td>
 																			</tr>
 																		</table>
@@ -107,7 +107,7 @@ global $base_url;
 																			<tr>
 																				<td valign="top" class="textContent">
 																					<div style="text-align:left;font-family:Helvetica,Arial,sans-serif;font-size:16px;margin-bottom:0;color:#333;line-height:150%;"><?php print $body ?></div>
-																					<div style="padding-top:10px;color:#333;font-family:Helvetica,Arial,sans-serif;font-size: 16px;line-height:150%;text-align:left;"><strong><?php print t('Your eMindhub team'); ?></strong></div>
+																					<div style="padding-top:10px;color:#333;font-family:Helvetica,Arial,sans-serif;font-size:16px;line-height:150%;text-align:left;"><strong><?php print t('Your eMindhub team'); ?></strong></div>
 																				</td>
 																			</tr>
 																		</table>
@@ -136,7 +136,7 @@ global $base_url;
 														<td align="center" valign="top" width="600" class="flexibleContainerCell">
 															<table border="0" cellpadding="30" cellspacing="0" width="100%">
 																<tr>
-																	<td bgcolor="#F2F2F2" style="padding-top:9px; padding-right:9px; padding-left:9px;" valign="top" align="center">
+																	<td bgcolor="#F2F2F2" style="padding-top:9px;padding-right:9px;padding-left:9px;" valign="top" align="center">
 						                        <table align="center" border="0" cellpadding="0" cellspacing="0">
 						                          <tbody><tr>
 						                            <td valign="top" align="center">
@@ -153,20 +153,20 @@ global $base_url;
 
                                         <table style="display:inline;" align="left" border="0" cellpadding="0" cellspacing="0">
                                           <tbody><tr>
-                                              <td style="padding-right:10px; padding-bottom:9px;" class="mcnFollowContentItemContainer" valign="top">
+                                              <td style="padding-right:10px;padding-bottom:9px;" class="mcnFollowContentItemContainer" valign="top">
                                                   <table class="mcnFollowContentItem" width="100%" border="0" cellpadding="0" cellspacing="0">
                                                       <tbody><tr>
-                                                          <td style="padding-top:5px; padding-right:10px; padding-bottom:5px; padding-left:9px;" valign="middle" align="left">
+                                                          <td style="padding-top:5px;padding-right:10px;padding-bottom:5px;padding-left:9px;" valign="middle" align="left">
                                                               <table width="" align="left" border="0" cellpadding="0" cellspacing="0">
                                                                   <tbody><tr>
 
                                                                           <td class="mcnFollowIconContent" valign="middle" width="24" align="center">
-                                                                              <a href="http://www.emindhub.com/?pk_campaign=website&amp;pk_kwd=picto" target="_blank"><img alt="eMindHub" src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/picto/picto_emindhub_small.png" style="display:block;" class="" height="24" width="24"></a>
+                                                                              <a href="<?php print $base_url; ?>/?pk_campaign=website&amp;pk_kwd=picto" target="_blank"><img alt="eMindHub" src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/picto/picto_emindhub_small.png" style="display:block;" class="" height="24" width="24"></a>
                                                                           </td>
 
 
                                                                           <td class="mcnFollowTextContent" style="padding-left:5px;" valign="middle" align="left">
-                                                                              <a href="http://www.emindhub.com/?pk_campaign=website&amp;pk_kwd=picto" target="" style="font-family: Helvetica; font-size: 12px; text-decoration: none; color: rgb(101, 101, 101);">Website</a>
+                                                                              <a href="<?php print $base_url; ?>/?pk_campaign=website&amp;pk_kwd=picto" target="" style="font-family:Helvetica;font-size:12px;text-decoration:none;color:rgb(101, 101, 101);">Website</a>
                                                                           </td>
 
                                                                   </tr>
@@ -188,10 +188,10 @@ global $base_url;
 
                                       <table style="display:inline;" align="left" border="0" cellpadding="0" cellspacing="0">
                                           <tbody><tr>
-                                              <td style="padding-right:0; padding-bottom:9px;" class="mcnFollowContentItemContainer" valign="top">
+                                              <td style="padding-right:0;padding-bottom:9px;" class="mcnFollowContentItemContainer" valign="top">
                                                   <table class="mcnFollowContentItem" width="100%" border="0" cellpadding="0" cellspacing="0">
                                                       <tbody><tr>
-                                                          <td style="padding-top:5px; padding-right:10px; padding-bottom:5px; padding-left:9px;" valign="middle" align="left">
+                                                          <td style="padding-top:5px;padding-right:10px;padding-bottom:5px;padding-left:9px;" valign="middle" align="left">
                                                               <table width="" align="left" border="0" cellpadding="0" cellspacing="0">
                                                                   <tbody><tr>
 
@@ -201,7 +201,7 @@ global $base_url;
 
 
                                                                           <td class="mcnFollowTextContent" style="padding-left:5px;" valign="middle" align="left">
-                                                                              <a href="https://www.linkedin.com/company/emindhub" target="" style="font-family: Helvetica; font-size: 12px; text-decoration: none; color: rgb(101, 101, 101);">LinkedIn</a>
+                                                                              <a href="https://www.linkedin.com/company/emindhub" target="" style="font-family:Helvetica;font-size:12px;text-decoration:none;color:rgb(101, 101, 101);">LinkedIn</a>
                                                                           </td>
 
                                                                   </tr>
