9e5bec53716fa834703a44c7cf191d7d47d02a4f
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-02-11 17:41:44 +0100

    CHG: global tpl for requests #599

--- sites/all/themes/emindhub/templates/node--question1.tpl.php
+++ sites/all/themes/emindhub/templates/node--question1.tpl.php
@@ -199,23 +199,7 @@
 		?>
 
 		<?php else : ?>
-
-		<div class="highlighted jumbotron">
-			<div class="container">
-				<div class="region region-highlighted row">
-					<section id="block-pcp-pcp-profile-percent-complete" class="block block-pcp col-sm-12 clearfix">
-						<h2 class="block-title"><span><?php print t('Please complete your profile to see this request'); ?></span></h2>
-						<div class="content">
-							<?php if (module_exists('pcp')) : ?>
-							<?php $pcp_block = module_invoke('pcp', 'block_view', 'pcp_profile_percent_complete'); ?>
-							<?php print render($pcp_block['content']); ?>
-							<?php else : ?>
-							<?php endif; ?>
-						</div>
-					</section> <!-- /.block -->
-				</div>
-			</div>
-		</div>
+		<?php require_once drupal_get_path('theme', 'emindhub').'/templates/includes/nodeProfileComplete.tpl.php'; ?>
 		<?php endif; ?>
 
 	</div>
