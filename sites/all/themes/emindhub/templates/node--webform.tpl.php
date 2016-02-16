c696974b71fe143954f3e71eb26a5ed9366626bd
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-02-16 16:29:09 +0100

    CHG: remove emh_user_is in survey #604

--- sites/all/themes/emindhub/templates/node--webform.tpl.php
+++ sites/all/themes/emindhub/templates/node--webform.tpl.php
@@ -220,7 +220,7 @@
 
 		<?php if (empty($submissions) || $submissions['is_draft']) : ?>
 
-		<?php if ( (!emh_user_is_business() || $node->uid == $user->uid) && $node->webform['status'] ) : ?>
+		<?php if ( (user_access('edit own webform submissions') || $node->uid == $user->uid) && $node->webform['status'] ) : ?>
 		<?php if (!empty($node->webform['components'])) : ?>
 		<div id="comments" class="<?php print $classes; ?> row section emh-fieldgroup-blue-title"<?php print $attributes; ?>>
 	    <h2 class="h3"><span><?php print t('Answer the survey') ?></span></h2>
