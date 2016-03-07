0c432e808bf7dbc82f59d6fd44d6a7a93c544744
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-03-07 06:13:48 +0100

    ADD: new Behat steps for form element detection

--- bdd/features/bootstrap/FeatureContext.php
+++ bdd/features/bootstrap/FeatureContext.php
@@ -1,13 +1,17 @@
 <?php
 
-use Drupal\DrupalExtension\Context\RawDrupalContext;
-use Drupal\DrupalExtension\Context\DrupalContext;
-use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Drupal\DrupalExtension\Context\RawDrupalContext,
+    Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext,
+    Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
 use Behat\MinkExtension\Context\MinkContext;
-use Behat\Behat\Hook\Scope\BeforeScenarioScope;
-use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope,
+    Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
@@ -163,6 +167,83 @@ class FeatureContext extends DrupalContext {
     }
     $user = user_load($this->users[$name]->uid);
     emh_points_move_points($node, $user, (int) $points);
+  }
+
+
+  /**
+   * Checks, that form element with specified label is visible on page.
+   *
+   * @Then /^(?:|I )should see an? "(?P<label>[^"]*)" form element$/
+   */
+  public function assertFormElementOnPage($label) {
+    $element = $this->getSession()->getPage();
+    $nodes = $element->findAll('css', '.form-item label');
+    foreach ($nodes as $node) {
+      if ($node->getText() === $label) {
+        if ($node->isVisible()) {
+          return;
+        }
+        else {
+          throw new \Exception("Form item with label \"$label\" not visible.");
+        }
+      }
+    }
+    throw new \Behat\Mink\Exception\ElementNotFoundException($this->getSession(), 'form item', 'label', $label);
+  }
+  /**
+   * Checks, that form element with specified label and type is visible on page.
+   *
+   * @Then /^(?:|I )should see an? "(?P<label>[^"]*)" (?P<type>[^"]*) form element$/
+   */
+  public function assertTypedFormElementOnPage($label, $type) {
+    $container = $this->getSession()->getPage();
+    $pattern = '/(^| )form-type-' . preg_quote($type). '($| )/';
+    $label_nodes = $container->findAll('css', '.form-item label');
+    foreach ($label_nodes as $label_node) {
+      // Note: getText() will return an empty string when using Selenium2D. This
+      // is ok since it will cause a failed step.
+      if ($label_node->getText() === $label
+        && preg_match($pattern, $label_node->getParent()->getAttribute('class'))
+        && $label_node->isVisible()) {
+        return;
+      }
+    }
+    throw new \Behat\Mink\Exception\ElementNotFoundException($this->getSession(), $type . ' form item', 'label', $label);
+  }
+  /**
+   * Checks, that element with specified CSS is not visible on page.
+   *
+   * @Then /^(?:|I )should not see an? "(?P<label>[^"]*)" form element$/
+   */
+  public function assertFormElementNotOnPage($label) {
+    $element = $this->getSession()->getPage();
+    $nodes = $element->findAll('css', '.form-item label');
+    foreach ($nodes as $node) {
+      // Note: getText() will return an empty string when using Selenium2D. This
+      // is ok since it will cause a failed step.
+      if ($node->getText() === $label && $node->isVisible()) {
+        throw new \Exception();
+      }
+    }
+  }
+  /**
+   * Checks, that form element with specified label and type is not visible on page.
+   *
+   * @Then /^(?:|I )should not see an? "(?P<label>[^"]*)" (?P<type>[^"]*) form element$/
+   */
+  public function assertTypedFormElementNotOnPage($label, $type) {
+    $container = $this->getSession()->getPage();
+    $pattern = '/(^| )form-type-' . preg_quote($type). '($| )/';
+    $label_nodes = $container->findAll('css', '.form-item label');
+    foreach ($label_nodes as $label_node) {
+      // Note: getText() will return an empty string when using Selenium2D. This
+      // is ok since it will cause a failed step.
+      if ($label_node->getText() === $label
+        && preg_match($pattern, $label_node->getParent()->getAttribute('class'))
+        && $label_node->isVisible()) {
+        throw new \Behat\Mink\Exception\ElementNotFoundException($this->getSession(), $type . ' form item', 'label', $label);
+      }
+    }
   }
 
   /**
