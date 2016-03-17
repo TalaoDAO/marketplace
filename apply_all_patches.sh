6981d8bf4c9652c17d744c73c875493df70fc9d4
Yoann Babel <yoann.babel@emindhub.com>
2016-03-17 17:03:28 +0100

    Revert "FIX: drush up --sercurity-only"
    This reverts commit 3d24f0e7a84d0c965f3279e566408fd329931b84.

--- apply_all_patches.sh
+++ apply_all_patches.sh
@@ -1,6 +1,6 @@
 drush up
-#DUPLICATES git apply -v patchs/drupal-7.x-Issue-2028713-by-axel.rutz__0.patch #https://www.drupal.org/node/2028713 Menu router does "too many SQL variables" on SQLite resulting in silent nondisplay of menu items
-#git apply -v patchs/bartick-menu-images.patch
+git apply -v patchs/drupal-7.x-Issue-2028713-by-axel.rutz__0.patch
+git apply -v patchs/bartick-menu-images.patch
 git apply -v patchs/mb_custom-settings-permission_2264913-1.patch --directory=sites/all/modules/contrib
 git apply -v patchs/entityreference_fields_do_not_validate-2249261-10.patch --directory=sites/all/modules/og/
 git apply -v patchs/schema-1850196-autoslave_incompatibility-4.patch --directory=sites/all/modules/debug/schema
