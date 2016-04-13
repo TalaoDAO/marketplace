e47bf63dee5513c2741a447000e3ae128afa61d1
Régis leroy <regis.leroy@makina-corpus.com>
2016-04-13 09:56:36 +0200

    Missing settings

--- templates/drupal/settings.php
+++ templates/drupal/settings.php
@@ -25,5 +25,8 @@ $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 $conf['allow_authorize_operations'] = FALSE;
 
+include_once('./includes/cache.inc');
+include_once('./sites/all/modules/contrib/memcache/memcache.inc');
+$conf['cache_default_class'] = 'MemCacheDrupal';
 $conf['memcache_key_prefix'] = '__MEMCACHE_PREFIX__';
 
