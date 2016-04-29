628466dc948d24fcb318369afda83905d644c2c5
Yoann Babel <yoann.babel@emindhub.com>
2016-04-29 16:25:57 +0200

    SYS: THEME: fix google fonts loading from HTTPS to avoid mixed content preventing load on production

--- www/sites/all/themes/emindhub/theme/html.func.php
+++ www/sites/all/themes/emindhub/theme/html.func.php
@@ -1,7 +1,7 @@
 <?php
 
 function emindhub_preprocess_html(&$variables) {
-	drupal_add_css('http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' , array('type' => 'external'));
+	drupal_add_css('https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' , array('type' => 'external'));
 
   global $user;
   foreach ( $user->roles as $role_id => $role ) {
