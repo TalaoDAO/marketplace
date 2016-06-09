3b0efd3b852b9c45d524354639e90f48b5668ad0
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-06-09 14:33:36 +0200

    CHG: video block HP #747

--- www/sites/all/themes/emindhub/templates/block/block--block--22.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--22.tpl.php
@@ -51,37 +51,37 @@ $front_theme = $base_url . '/' . $front_theme;
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="container">
-  <div class="row">
-    <div class="col-md-6 text-right">
-      <!-- Button trigger modal -->
-      <button type="button" id="hp-video" class="btn btn-default btn-lg" data-toggle="modal" data-target="#myModal">
-        <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
-      </button>
 
-      <!-- Modal -->
-      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="<?php print t('Close'); ?>"><span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" id="myModalLabel"><?php print $title; ?></h4>
-            </div>
-            <div class="modal-body">
-              <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/VAXPojC8KLU?rel=0&amp;showinfo=0"></iframe>
-              </div>
+    <div class="row">
+      <?php if ($title) : ?>
+      <div class="col-md-12">
+        <h2<?php print $title_attributes; ?>><span><?php print $title; ?></span></h2>
+      </div>
+      <?php endif; ?>
+      <div class="col-md-6 col-md-offset-3">
+        <button type="button" id="hp-video" class="btn btn-default btn-lg" data-toggle="modal" data-target="#myModal">
+          <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
+        </button>
+      </div>
+    </div>
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="<?php print t('Close'); ?>"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel"><?php print $title; ?></h4>
+          </div>
+          <div class="modal-body">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/VAXPojC8KLU?rel=0&amp;showinfo=0"></iframe>
             </div>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="content col-md-6">
-      <h2<?php print $title_attributes; ?>><span><?php print t('2 minutes <br />to discover eMindHub'); ?></span></h2>
-      <?php //print $content; ?>
-    </div>
-
-  </div>
+    <?php //print $content; ?>
   </div>
 
 </section> <!-- /.block -->
