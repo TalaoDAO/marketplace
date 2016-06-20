f80fdb278a9f006e0ef707e0859a55ab58df6023
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-06-20 09:52:23 +0200

    ADD: stop Youtube video when modal is closed #761

--- www/sites/all/themes/emindhub/js/main.js
+++ www/sites/all/themes/emindhub/js/main.js
@@ -67,6 +67,11 @@ function FileUpload() {
     //   html: true,
     // });
 
+    // Stop video in modal when you close it
+    $("#videoModal").on('hidden.bs.modal', function (e) {
+      $("#videoModal iframe").attr("src", $("#videoModal iframe").attr("src"));
+    });
+
   })
 })(jQuery);
 
