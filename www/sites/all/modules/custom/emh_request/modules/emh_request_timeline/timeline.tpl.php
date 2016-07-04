580305bc57cc69b6405378e89c04782fe5bb5dbc
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-07-04 18:18:35 +0200

    ADD: request timeline

--- www/sites/all/modules/custom/emh_request/modules/emh_request_timeline/timeline.tpl.php
+++ www/sites/all/modules/custom/emh_request/modules/emh_request_timeline/timeline.tpl.php
@@ -1,5 +1,11 @@
+<div class="created">
+  <?php print $created; ?>
+</div>
 <div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-    60%
+  <div class="progress-bar progress-bar-<?php print $status; ?>" role="progressbar" aria-valuenow="<?php print $progression; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php print $progression; ?>%;">
+    <?php print $left; ?>
   </div>
+</div>
+<div class="deadline">
+  <?php print $deadline; ?>
 </div>
