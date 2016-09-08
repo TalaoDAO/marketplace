d81e8ef5b1926910ff5163806b1c4195a8169713
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-09-08 11:05:16 +0200

    WIP: new email test

--- bdd/features/bootstrap/FeatureContext.php
+++ bdd/features/bootstrap/FeatureContext.php
@@ -311,4 +311,52 @@ class FeatureContext extends DrupalContext {
     }
   }
 
+  /**
+   * @Given /^the test email system is enabled$/
+   */
+  public function theTestEmailSystemIsEnabled() {
+    // Store the original system to restore after the scenario.
+    $this->originalMailSystem = variable_get('mail_system', array('default-system' => 'DefaultMailSystem'));
+    // Set the test system.
+    variable_set('mail_system', array('default-system' => 'TestingMailSystem'));
+    // Flush the email buffer, allowing us to reuse this step definition to clear existing mail.
+    variable_set('drupal_test_email_collector', array());
+  }
+
+  /**
+   * @Then /^the email to "([^"]*)" should contain "([^"]*)"$/
+   */
+  public function theEmailToShouldContain($to, $contents) {
+    // We can't use variable_get() because $conf is only fetched once per
+    // scenario.
+    $variables = array_map('unserialize', db_query("SELECT name, value FROM {variable} WHERE name = 'drupal_test_email_collector'")->fetchAllKeyed());
+    $this->activeEmail = FALSE;
+    foreach ($variables['drupal_test_email_collector'] as $message) {
+      if ($message['to'] == $to) {
+        $this->activeEmail = $message;
+        if (strpos($message['body'], $contents) !== FALSE ||
+          strpos($message['subject'], $contents) !== FALSE) {
+          return TRUE;
+        }
+        throw new \Exception('Did not find expected content in message body or subject.');
+      }
+    }
+    throw new \Exception(sprintf('Did not find expected message to %s', $to));
+  }
+
+  /**
+   * @Given /^the email should contain "([^"]*)"$/
+   */
+  public function theEmailShouldContain($contents) {
+    if (!$this->activeEmail) {
+      throw new \Exception('No active email');
+    }
+    $message = $this->activeEmail;
+    if (strpos($message['body'], $contents) !== FALSE ||
+      strpos($message['subject'], $contents) !== FALSE) {
+      return TRUE;
+    }
+    throw new \Exception('Did not find expected content in message body or subject.');
+  }
+
 }
