46efef2591356ef86d27c9fa5f81d94029a02640
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-09-14 15:35:13 +0200

    CHG: new image for HP #868

--- www/sites/all/themes/emindhub/templates/block/block--block--22.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--22.tpl.php
@@ -1,92 +1,26 @@
 <?php
-/**
- * @file
- * Default theme implementation to display a block.
- *
- * Available variables:
- * - $block->subject: Block title.
- * - $content: Block content.
- * - $block->module: Module that generated the block.
- * - $block->delta: An ID for the block, unique within each module.
- * - $block->region: The block region embedding the current block.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. The default values can be one or more of the
- *   following:
- *   - block: The current template type, i.e., "theming hook".
- *   - block-[module]: The module generating the block. For example, the user
- *     module is responsible for handling the default user navigation block. In
- *     that case the class would be 'block-user'.
- * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
- * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
- *
- * Helper variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- * - $block_zebra: Outputs 'odd' and 'even' dependent on each block region.
- * - $zebra: Same output as $block_zebra but independent of any block region.
- * - $block_id: Counter dependent on each block region.
- * - $id: Same output as $block_id but independent of any block region.
- * - $is_front: Flags true when presented in the front page.
- * - $logged_in: Flags true when the current user is a logged-in member.
- * - $is_admin: Flags true when the current user is an administrator.
- * - $block_html_id: A valid HTML ID and guaranteed unique.
- *
- * @see bootstrap_preprocess_block()
- * @see template_preprocess()
- * @see template_preprocess_block()
- * @see bootstrap_process_block()
- * @see template_process()
- *
- * @ingroup themeable
- */
-global $base_url, $language;
-$current_lang = $language->language;
+global $base_url;
 $front_theme = path_to_theme();
 $front_theme = $base_url . '/' . $front_theme;
-
-// Video
-if (!empty($current_lang) && $current_lang == 'fr') {
-  $video_url = 'https://www.youtube.com/embed/Vi2bkPyqyCs?&amp;hl=fr&amp;cc_lang_pref=fr&amp;cc_load_policy=1';
-} else {
-  $video_url = 'https://www.youtube.com/embed/VAXPojC8KLU';
-}
 ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="container">
 
     <div class="row">
-      <?php if ($title) : ?>
-      <div class="col-md-12">
-        <h2<?php print $title_attributes; ?>><span><?php print $title; ?></span></h2>
-      </div>
-      <?php endif; ?>
-      <div class="col-md-6 col-md-offset-3">
-        <button type="button" id="hp-video" class="btn btn-default btn-lg" data-toggle="modal" data-target="#videoModal">
-          <span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span>
-        </button>
-      </div>
-    </div>
 
-    <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="videoModalLabel">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="<?php print t('Close'); ?>"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="videoModalLabel"><?php print $title; ?></h4>
-          </div>
-          <div class="modal-body">
-            <div class="embed-responsive embed-responsive-16by9">
-              <iframe class="embed-responsive-item" src="<?php print $video_url; ?>"></iframe>
-            </div>
-          </div>
-        </div>
+      <div class="image">
+        <img alt="<?php print t('Services to communities'); ?>" class="center-block" src="<?php print $front_theme; ?>/images/content/block/eMindHub_Services-to_communities.jpg" />
       </div>
+
+      <div class="text">
+        <ol>
+          <li><?php print t('Post your request, it\'s <strong>free</strong>'); ?></li>
+          <li><?php print t('Add options to <strong>get the most from your request</strong> and <strong>ensure confidentiality</strong>'); ?></li>
+          <li><?php print t('<strong>Access expert profiles</strong> on the basis of the <strong>quality</strong> of their responses'); ?></li>
+        </ol>
+      </div>
+
     </div>
 
     <?php //print $content; ?>
