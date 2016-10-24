c59e4e31d234670177c8a32587289ac0e9c65788
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-10-24 09:34:00 +0200

    ADD: purchased count in theme

--- www/sites/all/themes/emindhub/templates/user/user-profile.tpl.php
+++ www/sites/all/themes/emindhub/templates/user/user-profile.tpl.php
@@ -34,13 +34,15 @@
  * @ingroup themeable
  */
 $uid = arg(1);
+$account = user_load($uid);
 $submission_count = emh_request_submission_get_user_submission_count($uid);
+$purchased_count = emh_user_profile_purchase_get_count($account);
 ?>
 <div class="profile"<?php print $attributes; ?>>
   <?php //print render($user_profile); ?>
 
   <div class="user-cartouche">
-    <?php if ($field_photo || $field_titre_metier || $field_entreprise || $field_address || $field_mail) : ?>
+    <?php if (!empty($user_profile['field_photo']) || !empty($user_profile['field_titre_metier']) || !empty($user_profile['field_entreprise']) || !empty($user_profile['field_address']) || !empty($user_profile['field_mail'])) : ?>
     <div class="cartouche-identity">
       <?php print render($user_profile['field_photo']); ?>
       <?php print render($user_profile['field_titre_metier']); ?>
@@ -51,10 +53,11 @@ $submission_count = emh_request_submission_get_user_submission_count($uid);
     <?php endif; ?>
     <div class="cartouche-activity">
       <span class="user-submission-count" title="<?php print $submission_count . ' ' . t('published submission(s)'); ?>"><?php print $submission_count; ?></span>
+      <span class="user-purchased-count" title="<?php print t('Profile purchased !count times', array('!count' => $purchased_count)); ?>"><?php print render($purchased_count); ?></span>
     </div>
   </div>
 
-  <?php if ($field_position || $field_working_status) : ?>
+  <?php if (!empty($user_profile['field_position']) || !empty($user_profile['field_working_status'])) : ?>
   <div class="group-organisation profile-section">
     <h2><span><?php print t('Organisation'); ?></span></h2>
     <?php print render($user_profile['field_position']); ?>
@@ -62,7 +65,7 @@ $submission_count = emh_request_submission_get_user_submission_count($uid);
   </div>
   <?php endif; ?>
 
-  <?php if ($field_needs_for_expertise || $field_specific_skills3) : ?>
+  <?php if (!empty($user_profile['field_needs_for_expertise']) || !empty($user_profile['field_specific_skills3'])) : ?>
   <div class="group-needs profile-section">
     <h2><span><?php print t('Needs'); ?></span></h2>
     <?php print render($user_profile['field_needs_for_expertise']); ?>
@@ -70,7 +73,7 @@ $submission_count = emh_request_submission_get_user_submission_count($uid);
   </div>
   <?php endif; ?>
 
-  <?php if ($field_domaine || $field_skills_set || $field_position_list || $field_employment_history || $field_cv) : ?>
+  <?php if (!empty($user_profile['field_domaine']) || !empty($user_profile['field_skills_set']) || !empty($user_profile['field_position_list']) || !empty($user_profile['field_employment_history']) || !empty($user_profile['field_cv'])) : ?>
   <div class="group-skills profile-section">
     <h2><span><?php print t('Skills & background'); ?></span></h2>
     <?php print render($user_profile['field_domaine']); ?>
@@ -81,7 +84,7 @@ $submission_count = emh_request_submission_get_user_submission_count($uid);
   </div>
   <?php endif; ?>
 
-  <?php if ($field_other_areas) : ?>
+  <?php if (!empty($user_profile['field_other_areas'])) : ?>
   <div class="group-interests profile-section">
     <h2><span><?php print t('Interests'); ?></span></h2>
     <?php print render($user_profile['field_other_areas']); ?>
