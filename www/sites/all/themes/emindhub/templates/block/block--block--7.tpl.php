c0f22b28044f36b498a9a2e93f522e01d87f8752
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-10-25 14:18:26 +0200

    Main markup & css for persona selector

--- www/sites/all/themes/emindhub/templates/block/block--block--7.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--7.tpl.php
@@ -11,6 +11,34 @@ if (!empty($current_lang) && $current_lang == 'fr') {
   $video_url = 'https://www.youtube.com/embed/VAXPojC8KLU';
 }
 ?>
+
+<section class="select-persona-wrapper">
+
+  <div class="select-persona container">
+    <div class="select-persona-title">
+      <?php print t('The best aviation experts answer your questions.'); ?>
+    </div>
+    <div class="select-persona-subtitle">
+      <?php print t('Your projects deserve the best expertise.'); ?>
+    </div>
+
+    <div class="row">
+
+      <div class="persona-customer col-md-6">
+        <a class="button" href="#"><?php print t('You are a customer?'); ?></a>
+        <p><?php print t('Découvrez comment nullam quis<br>lacinia augue. Curabitur'); ?></p>
+      </div>
+
+      <div class="persona-expert col-md-6">
+        <a class="button" href="#"><?php print t('You are an expert?'); ?></a>
+        <p><?php print t('Découvrez comment nullam quis<br>lacinia augue. Curabitur'); ?></p>
+      </div>
+
+    </div>
+  </div>
+
+</section><!-- /.select-persona-wrapper -->
+
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content container">
