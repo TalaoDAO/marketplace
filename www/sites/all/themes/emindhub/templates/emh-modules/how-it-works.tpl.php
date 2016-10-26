c27d03656765d11aa9d83c39a7bb0b237a13cb94
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-10-26 19:48:00 +0200

    Main markup for 'How it works?'

--- www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
@@ -1,9 +1,90 @@
-<section class="how-it-works container">
-    <div class="emh-title">
-        <?php print t('How it works?') ?>
-    </div>
-    <div class="emh-subtitle">
-        <?php print t('How it works?') ?>
+<section class="how-it-works hiw container">
+  <div class="emh-title">
+    <?php print t('CHANGEME Comment ça marche ?'); ?>
+  </div>
+  <div class="emh-subtitle">
+    <?php print t('CHANGEME C\'est simple, gratuit et en 3 étapes'); ?>
+  </div>
+
+  <ul class="hiw-tabs">
+    <li><button type="button" name="button" class="hiw-tab"><?php print t('You are a customer'); ?></button></li>
+    <li><button type="button" name="button" class="hiw-tab"><?php print t('You are an expert'); ?></button></li>
+  </ul>
+
+  <!-- CUSTOMER-->
+  <div class="hiw-customer">
+
+    <div class="hiw-step">
+      <div class="hiw-step-title">
+
+        <strong>1</strong> - <?php print t('CHANGEME Je choisis un type de service et une communauté d\'experts'); ?>
+      </div>
+
+      <ul class="hiw-services">
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">FAQ</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">Doc</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">Q/A</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">Call</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">Survey</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">Missions</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+        <li class="hiw-service">
+          <div class="icon"><img src="" alt="" /></div>
+          <div class="label">CV</div>
+          <div class="legend"><?php print t('CHANGEME Poser une question à la commnauté des experts'); ?></div>
+        </li>
+      </ul>
     </div>
 
+    <div class="hiw-step">
+      <div class="hiw-step-title">
+        <strong>2</strong> - <?php print t('CHANGEME Je consulte les réponses'); ?>
+      </div>
+
+      <img class="hiw-step-picture" src="" alt="" />
+    </div>
+
+    <div class="hiw-step">
+      <div class="hiw-step-title">
+        <strong>3</strong> - <?php print t('CHANGEME J\'accède au profil des experts et j\'achète leur contenu'); ?>
+      </div>
+
+      <img class="hiw-step-picture" src="" alt="" />
+    </div>
+
+    <a href="#"><?php print t('CHANGEME Voir le schéma complet') ?></a>
+
+    <a class="button" href="#"><?php print t('CHANGEME Poster une demande'); ?></a>
+
+
+  </div>
+
+
+  <!-- EXPERT-->
+  <div class="hiw-expert">
+
+  </div>
 </section>
