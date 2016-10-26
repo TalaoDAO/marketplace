12672b7028b94ee8e1880f8ebd5b0905bc3c0f0e
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-10-26 16:55:27 +0200

    Rename modules to emh-modules
    	to avoid confusion with Drupal modules

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
@@ -0,0 +1,9 @@
+<section class="how-it-works container">
+    <div class="emh-title">
+        <?php print t('How it works?') ?>
+    </div>
+    <div class="emh-subtitle">
+        <?php print t('How it works?') ?>
+    </div>
+
+</section>
