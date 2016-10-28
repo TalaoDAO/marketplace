85351fc0c1172307af519f88cef064d1f4c21de0
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-10-28 14:31:04 +0200

    Create base files for Expertise module

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/expertise.tpl.php
@@ -0,0 +1,9 @@
+<section class="emh-module expertise">
+  <div class="emh-title">
+    <?php print t('CHANGEME Domaine d\'expertise'); ?>
+  </div>
+  <div class="emh-subtitle">
+    <?php print t('CHANGEME Consultez les domaines où client et experts se retrouvent'); ?>
+  </div>
+
+</section>
