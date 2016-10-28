b7f548003db7e299c5b0a4955c64d6b120811225
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-10-28 17:17:23 +0200

    Create base files for Requests module

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/requests.tpl.php
@@ -0,0 +1,11 @@
+<section class="emh-module requests">
+  <div class="emh-title">
+    <?php print t('CHANGEME Demandes en cours'); ?>
+  </div>
+  <div class="emh-subtitle">
+    <?php print t('CHANGEME Consultez les dernières demandes des clients'); ?>
+  </div>
+
+
+
+</div>
