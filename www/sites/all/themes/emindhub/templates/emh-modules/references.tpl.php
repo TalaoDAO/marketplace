aab5d5aa6e66c69cf792505d6c721ba063fe2654
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-03 11:38:03 +0100

    References module

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/references.tpl.php
@@ -0,0 +1,13 @@
+<section class="emh-module references container">
+    <div class="emh-title">
+      <?php print t('CHANGEME Ils nous ont fait confiance'); ?>
+    </div>
+    <div class="emh-subtitle">
+      <?php print t('CHANGEME Rejoignez les entreprises qui utilisent l\'expertise eMindHub'); ?>
+    </div>
+
+    <ul class="references-list">
+        <!-- <li><a href="#"><?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/enac.png" alt="ENAC" /></a></li> -->
+        <li><a href="#"><img src="http://i.imgur.com/hpnUaZ6.png" alt="" /></a></li>
+    </ul>
+</section>
