e3ef159118f346373acdd4a485e84495161e0446
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-04 14:21:16 +0100

    Main style for customer-emh-expert module

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/customer-emh-expert.tpl.php
@@ -0,0 +1,38 @@
+<div class="emh-module customer-emh-expert container">
+
+    <div class="cee-customer cee-block">
+        <div class="title">
+            <?php print t('Customers') ?>
+        </div>
+        <ul class="cee-items">
+            <li>azerty</li>
+            <li>qsdfgh sdg fdsth</li>
+            <li>wxcvbn</li>
+            <li>mlkjhg</li>
+        </ul>
+    </div>
+
+    <div class="cee-emh cee-block">
+        <div class="title">
+            <img class="horizontal" src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/logo/logo-h.svg" alt="eMindHub" />
+            <img class="vertical"   src="<?php print $base_url . '/' . drupal_get_path('theme', 'emindhub'); ?>/images/logo/logo-v.svg" alt="eMindHub" />
+        </div>
+        <ul class="cee-items">
+            <li>azerty</li>
+            <li>qsdfgh</li>
+            <li>wxcvbn</li>
+        </ul>
+    </div>
+
+    <div class="cee-expert cee-block">
+        <div class="title">
+            <?php print t('Experts') ?>
+        </div>
+        <ul class="cee-items">
+            <li>azerty</li>
+            <li>qsdf gh</li>
+            <li>wxcvbn</li>
+        </ul>
+    </div>
+
+</div>
