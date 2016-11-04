cfbb39f81869ee867881c4bafda2b4b9cb57677a
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-04 16:03:36 +0100

    CHG: blocks cleanup + ADD: new custom front blocks

---
+++ www/sites/all/themes/emindhub/templates/block/block--block--26.tpl.php
@@ -0,0 +1,8 @@
+<?php
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/persona.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/emhlive.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/how-it-works.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/expertise.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/requests.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/publications.tpl.php');
+?>
