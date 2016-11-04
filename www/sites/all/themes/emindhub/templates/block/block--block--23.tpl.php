09b1dc9b78c94f8f82906e198948c1d06e12f6b3
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-04 16:03:36 +0100

    CHG: blocks cleanup + ADD: new custom front blocks

---
+++ www/sites/all/themes/emindhub/templates/block/block--block--23.tpl.php
@@ -0,0 +1,2 @@
+<?php
+include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/school-community.tpl.php');
---
+++ www/sites/all/themes/emindhub/templates/block/block--block--27.tpl.php
@@ -0,0 +1,2 @@
+<?php
+include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/references.tpl.php');
