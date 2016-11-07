c86cca03a789d7cec0c3477b72eb66f72c5e09d2
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-07 16:10:00 +0100

    WIP

--- www/sites/all/themes/emindhub/templates/block/block--block--27.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--27.tpl.php
@@ -59,3 +59,15 @@
    */
   </script>
 </section>
+
+<?php
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/persona.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/emhlive.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/how-it-works.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/expertise.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/requests.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/school-community.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/publications.tpl.php');
+  // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/references.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/customer-emh-expert.tpl.php');
+?>
