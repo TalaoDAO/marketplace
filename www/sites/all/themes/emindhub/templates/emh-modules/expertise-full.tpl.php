adee6e300619015b8a33d271aaf344c0c7d758b6
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-08 12:17:53 +0100

    Markup, styles & images for expertise full display

--- www/sites/all/themes/emindhub/templates/emh-modules/expertise-full.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/expertise-full.tpl.php
@@ -1,4 +1,4 @@
-<section class="emh-module expertise-full">
+<section class="emh-module container expertise-full">
   <div class="emh-title">
     <?php print t('CHANGEME Domaine d\'expertise'); ?>
   </div>
@@ -8,10 +8,12 @@
 
   <div class="expertise-list">
 
-    <?php $expertiseLoop = 4; while ($expertiseLoop--): ?>
-    <div class="expertise-list-item">
+    <?php $expertise = ['transverse', 'technologies', 'disciplines', 'produits']; $expertiseLoop = 4; while ($expertiseLoop--): ?>
+    <div class="expertise-list-item expertise-<?php print $expertise[$expertiseLoop]; ?>">
       <div class="domain">
-        <?php print t('Produits'); ?>
+        <div class="domain-name">
+          <?php print t('Produits'); ?>
+        </div>
       </div>
       <ul class="subdomains">
         <li><?php print t('Air traffic Management'); ?></li>
@@ -23,6 +25,11 @@
       </ul>
       <div class="description">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+        <div class="links">
+          <a class="request" href="#"><?php print t('CHANGEME Poster une demande'); ?></a>
+          <a class="expertise" href="#"><?php print t('CHANGEME Proposer une expertise'); ?></a>
+        </div>
       </div>
 
     </div>
