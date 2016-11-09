29b20d969832a1dccff8d32884549a61efdab5d8
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-09 09:56:24 +0100

    Empty files for person-list module
    	previously named id-list

--- www/sites/all/themes/emindhub/templates/block/block--block--26.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--26.tpl.php
@@ -33,6 +33,7 @@
 <?php
   // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/persona.tpl.php');
   // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/emhlive.tpl.php');
+  include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/person-list.tpl.php');
   // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/how-it-works.tpl.php');
   // include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/expertise.tpl.php');
   include_once(drupal_get_path('theme','emindhub').'/templates/emh-modules/requests.tpl.php');
