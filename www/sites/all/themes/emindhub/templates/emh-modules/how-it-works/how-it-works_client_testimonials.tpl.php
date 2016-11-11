eb30841bddab97bbb2cdc35b4b860b8a13908a80
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-11 18:52:10 +0100

    ADD: How it works & FAQ styles

---
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works/how-it-works_client_testimonials.tpl.php
@@ -0,0 +1,104 @@
+<section class="emh-module testimonial container">
+
+    <ul class="testimonial-tabs">
+        <li class="active"><?php print t('Managers'); ?></li>
+        <li><?php print t('Project leaders'); ?></li>
+        <li><?php print t('Engineers'); ?></li>
+        <li><?php print t('Consultants'); ?></li>
+    </ul>
+
+    <ul class="testimonial-slider">
+        <li class="testimonial-slider-item">
+            <div class="content">
+
+                <div class="logo">
+                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
+                </div>
+
+                <div class="person"><!-- identique à ceux du module person-list -->
+                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
+                    <div class="content">
+                        <div class="name">Hervé PIERRET</div>
+                        <div class="position">DG AIR CORSICA</div>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <?php print t('We have used the services of EMindHub several times, and each time we made a request, we got quick and relevant responses.'); ?>
+                </blockquote>
+
+            </div>
+
+        </li>
+
+        <li class="testimonial-slider-item">
+            <div class="content">
+
+                <div class="logo">
+                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
+                </div>
+
+                <div class="person"><!-- identique à ceux du module person-list -->
+                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
+                    <div class="content">
+                        <div class="name">Patrick LANGLADE</div>
+                        <div class="position">Partner THEANO ADVISORS</div>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <?php print t('We searched for several weeks and without success for a very specific profile. With EMindHub, we found it in 24 hours.'); ?>
+                </blockquote>
+
+            </div>
+
+        </li>
+
+        <li class="testimonial-slider-item">
+            <div class="content">
+
+                <div class="logo">
+                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
+                </div>
+
+                <div class="person"><!-- identique à ceux du module person-list -->
+                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
+                    <div class="content">
+                        <div class="name">Jérôme AILLET</div>
+                        <div class="position">Ingénieur d'affaires AKKA TECHNOLOGIES</div>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <?php print t('I was impressed by how quickly the responses came through and the quality of the experts who positioned themselves. We have already involved several of them with our clients.'); ?>
+                </blockquote>
+
+            </div>
+
+        </li>
+
+        <li class="testimonial-slider-item">
+            <div class="content">
+
+                <div class="logo">
+                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
+                </div>
+
+                <div class="person"><!-- identique à ceux du module person-list -->
+                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
+                    <div class="content">
+                        <div class="name">Thierry PORRO</div>
+                        <div class="position">Consultant freelance en asset management</div>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <?php print t('EMindHub has allowed me to identify profiles of experts to respond to a proposal for a client, and at the same time, expand my network of potential partners.'); ?>
+                </blockquote>
+
+            </div>
+
+        </li>
+    </ul>
+
+</section>
