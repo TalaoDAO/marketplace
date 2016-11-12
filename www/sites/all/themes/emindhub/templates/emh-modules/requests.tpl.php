13c5d28195196b51e4d6501e11f3991435b979d3
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-12 19:24:29 +0100

    Move JS block for Requests to dedicated file

--- www/sites/all/themes/emindhub/templates/emh-modules/requests.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/requests.tpl.php
@@ -81,47 +81,4 @@
 
   </div>
 
-  <script type="text/javascript">
-    /**
-     * https://github.com/kenwheeler/slick/
-     */
-    var requestsSliderOptions = {
-      arrows: false,
-      dots: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
-      infinite: true,
-      appendDots: '.requests .emh-dots',
-      responsive: [
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-
-    /**
-     * Debounce postpone execution on event trigger stop
-     * then enable slider only if width <= 800
-     */
-    var checkRequestsSlick = debounce(function () {
-      if (document.body.clientWidth > 800) {
-        jQuery('.requests-slider .row.slick-initialized').slick('unslick');
-      } else {
-        jQuery('.requests-slider .row:not(.slick-initialized)').slick(requestsSliderOptions);
-      }
-    }, 300);
-
-    checkRequestsSlick();
-    window.addEventListener('resize', checkRequestsSlick);
-
-    /**
-     * @TODO
-     * Externaliser ce script dans un fichier externe
-     */
-  </script>
-
 </section>
