dddd03b64e5fa11dfe45b5f522257f2819b0d5c6
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-12 13:06:07 +0100

    Improve testimonial slider JS (use Drupal.behaviors)
    	Refs #910

--- www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
@@ -67,28 +67,46 @@
   </script>
 
   <script type="text/javascript">
-    /**
-     * https://github.com/kenwheeler/slick/
-     */
-    jQuery('.testimonial-slider').slick({
-      infinite: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: false,
-      vertical: true
-    });
+    Drupal.behaviors.testimonials = {
+      attach: function (context, settings) {
 
-    window.addEventListener('resize', debounce(function () {
-        jQuery('.testimonial-slider.slick-initialized').slick('refresh');
-    }, 300));
+        /**
+         * Manage each slider individualy
+         */
+        jQuery('.testimonial-slider', context).once().each(function () {
+          var $slider = jQuery(this);
 
-    jQuery('.testimonial-tabs').on('click', 'li', function (e) {
-        var $this = jQuery(this);
-        jQuery('.testimonial-slider').slick('slickGoTo', $this.index());
-        $this.addClass('active').siblings().removeClass('active');
-    });
+          /**
+           * Init slider
+           */
+          $slider.slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            vertical: true
+          });
 
+          /**
+           * Refresh slider on window resize
+           */
+          window.addEventListener('resize', debounce(function () {
+              $slider.slick('refresh');
+          }, 300));
+
+          /**
+           * Switch slider active item when using tabs
+           */
+          $slider.siblings('.testimonial-tabs').on('click', 'li', function (e) {
+              var $this = jQuery(this);
+              $slider.slick('slickGoTo', $this.index());
+              $this.addClass('active').siblings().removeClass('active');
+          });
+
+        });
+      }
+    };
   </script>
 
 </section>
