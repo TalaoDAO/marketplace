36cae457bab039a1d1802abc6ca25bcce993e816
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-12 20:10:53 +0100

    Move JS block for EMHLive to dedicated file

--- www/sites/all/themes/emindhub/templates/emh-modules/emhlive/publications/block--views--58462e19e23ccfccdcc6b91a103ef1cd.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/emhlive/publications/block--views--58462e19e23ccfccdcc6b91a103ef1cd.tpl.php
@@ -17,37 +17,6 @@
 
     <div class="emh-dots emh-dots-alt below"></div>
 
-    <script type="text/javascript">
-      /**
-       * https://github.com/kenwheeler/slick/
-       */
-      jQuery('.emhlive-slider').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        appendArrows: '.emhlive-arrows',
-        dots: true,
-        appendDots: '.emhlive .emh-dots.above',
-        responsive: [{
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2
-            // appendDots: '.emhlive-dots.below',
-          }
-        }, {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            appendDots: '.emhlive .emh-dots.below'
-          }
-        }]
-      });
-
-      /**
-       * @TODO
-       * Externaliser ce script dans un fichier externe
-       */
-    </script>
   <?php endif;?>
 
 </section> <!-- /.block -->
