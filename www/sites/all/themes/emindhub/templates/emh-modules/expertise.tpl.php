e40f5b649d9e3749fae5ec2666b748212e544ff7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-12 19:56:06 +0100

    Move JS block for Expertise to dedicated file

--- www/sites/all/themes/emindhub/templates/emh-modules/expertise.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/expertise.tpl.php
@@ -39,39 +39,4 @@
 
   <div class="emh-dots emh-dots-alt"></div>
 
-  <script type="text/javascript">
-    /**
-     * https://github.com/kenwheeler/slick/
-     */
-    jQuery('.expertise-slider').slick({
-      infinite: false,
-      arrows: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      dots: true,
-      appendDots: '.expertise .emh-dots',
-      responsive: [{
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3
-        }
-      }, {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2
-        }
-      }, {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-        }
-      }]
-    });
-  </script>
-
 </section>
