5093e3a1c5add64b26bc13c5f30659809e6ee1db
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-11-12 18:47:00 +0100

    Move JS block for FAQ to dedicated file

--- www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works.tpl.php
@@ -44,29 +44,6 @@
   </script>
 
   <script type="text/javascript">
-      Drupal.behaviors.faq = {
-          attach: function (context, settings) {
-              jQuery('.emh-module.faq', context).once(function (e) {
-                  var $this = jQuery(this);
-
-                  /**
-                   * Initialy close all answers
-                   */
-                  $this.find('.answer').hide();
-
-                  /**
-                   * Add click event to questions
-                   */
-                  $this.find('.question').on('click', function () {
-                      jQuery(this).next().slideToggle().siblings('.answer').slideUp();
-                      jQuery(this).toggleClass('active').siblings('.question').removeClass('active');
-                  });
-              });
-          }
-      };
-  </script>
-
-  <script type="text/javascript">
     Drupal.behaviors.testimonials = {
       attach: function (context, settings) {
 
