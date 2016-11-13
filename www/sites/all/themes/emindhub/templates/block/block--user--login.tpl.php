baf644fcab1673559adc8d5fde287a2928b21c4e
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-13 17:09:44 +0100

    ADD: wording + forms

--- www/sites/all/themes/emindhub/templates/block/block--user--login.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--user--login.tpl.php
@@ -1,7 +1,7 @@
 <section id="<?php print $block_html_id; ?>" class="emh-module <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content">
-    <ul class="nav navbar-nav navbar-emh-separator">
+    <ul class="nav navbar-nav">
       <li>
         <a tabindex="0" id="signUp" class="user-menu sign-up" data-placement="bottom" data-html="true" title="<?php print t('Registration'); ?>" data-template='<div class="popover signUp" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content signUpContent"></div></div>'><?php print t('Registration'); ?></a>
         <div id="signUpContent" class="signUpContent" style="display: none;">
@@ -11,6 +11,7 @@
           <span><a class="signin-expert" href="<?php print url('expert/register'); ?>"><?php print t('You\'re an expert'); ?></a></span>
         </div>
       </li>
+      <li><span class="navbar-text">|</span></li>
       <li>
         <a tabindex="1" id="signIn" class="user-menu sign-in" data-placement="bottom" data-html="true" title="<?php print t('Connexion'); ?>" data-template='<div class="popover signIn" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content signInContent"></div></div>'><?php print t('Connexion'); ?></a>
         <div id="signInContent" class="signInContent" style="display: none;">
