e575bbd310140ebcb4ca3e5f6c4c541478acedf7
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-13 14:52:47 +0100

    ADD: publications page theme

--- www/sites/all/themes/emindhub/theme/nodes.func.php
+++ www/sites/all/themes/emindhub/theme/nodes.func.php
@@ -1,6 +1,7 @@
 <?php
 
 function emindhub_preprocess_node(&$variables, $hook) {
+  global $user;
   $node = $variables['node'];
 
 	if (isset($node->type)) {
@@ -9,9 +10,6 @@ function emindhub_preprocess_node(&$variables, $hook) {
 
 		switch ($node->type) {
       case 'request':
-			case 'question1':
-			case 'webform':
-			case 'challenge':
 
 				// Views navigation between nodes
 				if (!empty($variables['elements']['links']['views_navigation']['#links']['back'])) {
@@ -36,44 +34,43 @@ function emindhub_preprocess_node(&$variables, $hook) {
 				// Request status
         $variables['request_status'] = emh_request_get_status($node->nid);
 
-        if ($node->type == 'request') {
-          global $user;
-
-          module_load_include('inc', 'webform', 'includes/webform.submissions');
-
-          $variables['submissions'] = webform_get_submissions(array('nid' => $node->nid));
-          foreach ($variables['submissions'] as $submission) {
-            if ($submission->uid == $user->uid) {
-              $variables['user_submission'] = $submission;
-              $variables['submission_status'] = emh_request_submission_get_status($submission);
-              break;
-            }
-          }
-
-          $variables['hidden_name'] = false;
-          if ($hide_name = field_get_items('node', $node, 'field_hide_name')) {
-            $variables['hidden_name'] = $hide_name[0]['value'];
-          }
-
-          $variables['hidden_organisation'] = false;
-          if ($hide_organisation = field_get_items('node', $node, 'field_hide_organisation')) {
-            $variables['hidden_organisation'] = $hide_organisation[0]['value'];
+        // Submission status
+        module_load_include('inc', 'webform', 'includes/webform.submissions');
+        $variables['submissions'] = webform_get_submissions(array('nid' => $node->nid));
+        foreach ($variables['submissions'] as $submission) {
+          if ($submission->uid == $user->uid) {
+            $variables['user_submission'] = $submission;
+            $variables['submission_status'] = emh_request_submission_get_status($submission);
+            break;
           }
         }
 
+        $variables['hidden_name'] = false;
+        if ($hide_name = field_get_items('node', $node, 'field_hide_name')) {
+          $variables['hidden_name'] = $hide_name[0]['value'];
+        }
+
+        $variables['hidden_organisation'] = false;
+        if ($hide_organisation = field_get_items('node', $node, 'field_hide_organisation')) {
+          $variables['hidden_organisation'] = $hide_organisation[0]['value'];
+        }
+
 				break;
+
+      default:
+        break;
 		}
 	}
 }
 
-function emindhub_node_view_alter( &$build ) {
+function emindhub_node_view_alter(&$build) {
   // remove "add comment" link from node teaser mode display
   unset($build['links']['comment']['#links']['comment-add']);
   // and if logged out this will cause another list item to appear, so let's get rid of that
   unset($build['links']['comment']['#links']['comment_forbidden']);
 }
 
-// function emindhub_comment_view_alter( &$build ) {
+// function emindhub_comment_view_alter(&$build) {
 //
 // 	// echo '<pre>' . print_r($build['links'], TRUE) . '</pre>';
 //   // if ($build['#view_mode'] == 'teaser') {
