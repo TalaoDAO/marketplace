82d9a9e1dcbdebc2094a0e14e5bf103a80dba2e3
Laetitia Debruyne <pro@laetitiadebruyne.com>
2016-11-13 17:09:44 +0100

    ADD: wording + forms

--- www/sites/all/themes/emindhub/templates/emh-modules/how-it-works/how-it-works_expert_testimonials.tpl.php
+++ www/sites/all/themes/emindhub/templates/emh-modules/how-it-works/how-it-works_expert_testimonials.tpl.php
@@ -1,6 +1,6 @@
 <div class="emh-subtitle"><?php echo t('Sharing my eMindHub experience') ?></div>
 
-<section class="emh-module testimonial testimonial-alt container">
+<section class="emh-module testimonial testimonial-alt hiw-testimonial container">
 
     <ul class="testimonial-tabs">
         <li class="active"><?php print t('Employed'); ?></li>
@@ -16,34 +16,11 @@
                     <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
                 </div>
 
-                <div class="person"><!-- identique à ceux du module person-list -->
-                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
-                    <div class="content">
-                        <div class="name">Prénom nom</div>
-                        <div class="position">Lorem ipsum dolor sit amet.</div>
-                    </div>
-                </div>
-
-                <blockquote>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore<!--   magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
-                </blockquote>
-
-            </div>
-
-        </li>
-
-        <li class="testimonial-slider-item">
-            <div class="content">
-
-                <div class="logo">
-                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
-                </div>
-
-                <div class="person"><!-- identique à ceux du module person-list -->
+                <div class="person">
                     <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
                     <div class="content">
                         <div class="name">Philippe LE FUR</div>
-                        <div class="position">Expert gestion de projet et mécanique de précision</div>
+                        <div class="position"><?php print t('Expert in project management and precision mechanics'); ?></div>
                     </div>
                 </div>
 
@@ -62,11 +39,34 @@
                     <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
                 </div>
 
-                <div class="person"><!-- identique à ceux du module person-list -->
+                <div class="person">
+                    <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
+                    <div class="content">
+                        <div class="name">Philippe LE FUR</div>
+                        <div class="position"><?php print t('Expert in project management and precision mechanics'); ?></div>
+                    </div>
+                </div>
+
+                <blockquote>
+                    <?php print t('I like the speed and the opportunities of networking on EMindHub. I knew about the site via my network and I do, myself, sponsor others.'); ?>
+                </blockquote>
+
+            </div>
+
+        </li>
+
+        <li class="testimonial-slider-item">
+            <div class="content">
+
+                <div class="logo">
+                    <img src="https://dummyimage.com/200x100/000/fff.jpg" alt="" />
+                </div>
+
+                <div class="person">
                     <div class="picture"><img src="https://dummyimage.com/100/000/fff.jpg" alt="" /></div>
                     <div class="content">
                         <div class="name">Daniel OTTAWA</div>
-                        <div class="position">Expert retraité en supply chain</div>
+                        <div class="position"><?php print t('Retired expert in supply chain'); ?></div>
                     </div>
                 </div>
 
