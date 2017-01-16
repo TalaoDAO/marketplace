07eeae3d7f79e00ab50ff69ca0933025a168e2ab
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-01-16 20:04:09 +0100

    #1019 Menu Body : Join circles pour role Business

--- www/sites/all/themes/emindhub/templates/block/block--block--8.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--8.tpl.php
@@ -68,7 +68,7 @@ $safe_link = rawurldecode($base_url . '/requests/all?type[request]=request');
       <a class="btn btn-flash icon-user" href="<?php print url('invitations'); ?>"><?php print t('Invite experts'); ?></a>
     <?php endif; ?>
 
-    <?php if (user_has_role(3) || user_has_role(4) || user_has_role(6)) : ?>
+    <?php if (user_has_role(3) || user_has_role(4) || user_has_role(5) || user_has_role(6)) : ?>
       <a class="btn btn-flash icon-community" href="<?php print url('circles'); ?>"><?php print t('Join circles'); ?></a>
     <?php endif; ?>
 
