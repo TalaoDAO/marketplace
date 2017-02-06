4a473bbc4aa9ed54cf749d07e17eb2b14a8454ba
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-06 06:03:38 +0100

    ADD: tooltip in user badge

--- www/sites/all/themes/emindhub/templates/user/emh-user-badge.tpl.php
+++ www/sites/all/themes/emindhub/templates/user/emh-user-badge.tpl.php
@@ -1,7 +1,7 @@
 <div class="user-badge">
   <span  class="user-photo">
     <?php if ($user_profile_link) : ?>
-    <a href="<?php print $user_profile_link; ?>" alt="<?php print $identity; ?>" title="<?php print $identity; ?>">
+    <a href="<?php print $user_profile_link; ?>" alt="<?php print $identity; ?>" data-toggle="tooltip" data-placement="top" title="<?php print $identity; ?>">
       <?php print $photo; ?>
     </a>
     <?php else : ?>
