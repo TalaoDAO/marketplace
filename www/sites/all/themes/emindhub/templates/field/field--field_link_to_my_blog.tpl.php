246fc09bb84366e7d0e3e32ceb26260bb26e1690
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-02-07 14:08:43 +0100

    #1049 #1044 profile view (WIP) & fix warnings

--- www/sites/all/themes/emindhub/templates/field/field--field_link_to_my_blog.tpl.php
+++ www/sites/all/themes/emindhub/templates/field/field--field_link_to_my_blog.tpl.php
@@ -5,14 +5,7 @@
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
-
-        <?php $profileURL = emindhub_beautiful_user_profile_link();
-        if (!empty($profileURL)) : ?>
-        <?php print t('eMindhub profile'); ?>&nbsp;:&nbsp;<?php print emindhub_beautiful_user_profile_link(); ?>&nbsp;|&nbsp;<?php print t('Website'); ?>&nbsp;:&nbsp;<?php print render($item); ?>
-        <?php else : ?>
         <?php print t('Website'); ?>&nbsp;:&nbsp;<?php print render($item); ?>
-        <?php endif; ?>
-
     </div>
     <?php endforeach; ?>
   </div>
