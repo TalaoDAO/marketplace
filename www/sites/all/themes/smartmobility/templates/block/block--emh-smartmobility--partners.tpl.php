91450379858c0301a0f4259a2a9a69a54c2f41d7
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-20 12:13:21 +0100

    ADD: Smart Mobility module & theme #1055

---
+++ www/sites/all/themes/smartmobility/templates/block/block--emh-smartmobility--partners.tpl.php
@@ -0,0 +1,16 @@
+<?php global $base_url; ?>
+
+<section id="<?php print $block_html_id; ?>" class="emh-module references <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if ($title): ?>
+    <div class="emh-subtitle">
+      <?php print $title; ?>
+    </div>
+  <?php endif;?>
+
+  <ul class="references-list">
+      <li><img src="<?php print $base_url . '/' . drupal_get_path('theme', 'smartmobility'); ?>/images/alixio_logo.png" alt="Alixio" /></li>
+      <li><img src="<?php print $base_url . '/' . drupal_get_path('theme', 'smartmobility'); ?>/images/emindhub_logo.svg" alt="EmindHub" /></li>
+  </ul>
+
+</section>
