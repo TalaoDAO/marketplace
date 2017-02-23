3a0960a963c6600473752edafbc659a7b79ee1e2
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-02-24 00:33:35 +0100

    #1079 _emh_request_submission_get_comment($sid)

--- www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
+++ www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
@@ -15,12 +15,27 @@
 
 <span class="date"><?php print t('Submitted: !date', array('!date' => check_plain(format_date($submission->submitted, webform_variable_get('webform_date_type'))))); ?></span>
 <p class="text-center"><?php print flag_create_link('interesting_answer', $submission->sid); ?><?php print flag_create_link('comment_answer', $submission->sid); ?></p>
+<h3>Your feedback comment</h3>
+<blockquote>
+  <?php print _emh_request_submission_get_comment($submission->sid); ?>
+</blockquote>
 
 <?php
-//TODO: how to get this entity_id ?
-$entity_id = 163; // or else for you
-$flagging = flagging_load($entity_id);
-$comment_answer = $flagging->field_comment_answer[LANGUAGE_NONE][0]['value'];
+
+/**
+ * Helper function to get a submission comment.
+ */
+function _emh_request_submission_get_comment($sid) {
+  $query = db_select('flagging', 'f');
+  $query->join('field_data_field_comment_answer', 'c', 'f.flagging_id = c.entity_id');
+  $query
+    ->condition('f.entity_type', 'webform_submission')
+    ->condition('f.fid', 16)
+    ->condition('f.entity_id', $sid)
+    ->fields('c', array('field_comment_answer_value'));
+  $result = $query->execute();
+  $row = $result->fetch();
+
+  return ($row->field_comment_answer_value);
+}
 ?>
-<h3>Your feedback comment</h3>
-<blockquote><?php print $comment_answer; ?></blockquote>
