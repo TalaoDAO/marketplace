af9588ff376d60faa50ca860e98e16f73c024cf6
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-23 17:13:06 +0100

    ADD: working connexion modal #1070

---
+++ www/sites/all/modules/custom/emh_smartmobility/theme/block--emh-smartmobility--persona.tpl.php
@@ -0,0 +1,8 @@
+<section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $title; ?>
+  <?php print t('Une initiative dédiée au redéploiement des salariés d\'Airbus dans le secteur de l\'aéronautique'); ?>
+  <?php print t('Vous êtes un partenaire'); ?>
+  <?php print t('Vous êtes salarié Airbus'); ?>
+
+</section><!-- /.select-persona-wrapper -->
