b30b103255a710beacfb4223d2fe5823ee3e501f
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-02-23 21:58:19 +0100

    #1079 stuck ATM

--- www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
+++ www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
@@ -15,3 +15,12 @@
 
 <span class="date"><?php print t('Submitted: !date', array('!date' => check_plain(format_date($submission->submitted, webform_variable_get('webform_date_type'))))); ?></span>
 <p class="text-center"><?php print flag_create_link('interesting_answer', $submission->sid); ?><?php print flag_create_link('comment_answer', $submission->sid); ?></p>
+
+<?php
+//TODO: how to get this entity_id ?
+$entity_id = 163; // or else for you
+$flagging = flagging_load($entity_id);
+$comment_answer = $flagging->field_comment_answer[LANGUAGE_NONE][0]['value'];
+?>
+<h3>Your feedback comment</h3>
+<blockquote><?php print $comment_answer; ?></blockquote>
