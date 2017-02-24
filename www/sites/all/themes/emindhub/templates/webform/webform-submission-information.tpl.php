1ab04a873b42b0a2a9fe90cdc31b1fc87f18417b
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-24 07:50:54 +0100

    WIP

--- www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
+++ www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
@@ -14,4 +14,9 @@
 ?>
 
 <span class="date"><?php print t('Submitted: !date', array('!date' => check_plain(format_date($submission->submitted, webform_variable_get('webform_date_type'))))); ?></span>
-<p class="text-center"><?php print flag_create_link('interesting_answer', $submission->sid); ?></p>
+
+<?php if (!empty(flag_create_link('interesting_answer', $submission->sid))) : ?>
+<p id="submission-flags">
+  <?php print flag_create_link('interesting_answer', $submission->sid); ?>
+</p>
+<?php endif; ?>
