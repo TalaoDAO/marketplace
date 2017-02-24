87734871622458f9bcf8d866277ddd0b43820b36
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-02-24 05:20:42 +0100

    #1079 export comment text field, get proper flag id, move helper function to module and pass the comment to the template

--- www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
+++ www/sites/all/themes/emindhub/templates/webform/webform-submission-information.tpl.php
@@ -15,27 +15,7 @@
 
 <span class="date"><?php print t('Submitted: !date', array('!date' => check_plain(format_date($submission->submitted, webform_variable_get('webform_date_type'))))); ?></span>
 <p class="text-center"><?php print flag_create_link('interesting_answer', $submission->sid); ?><?php print flag_create_link('comment_answer', $submission->sid); ?></p>
-<h3>Your feedback comment</h3>
-<blockquote>
-  <?php print _emh_request_submission_get_comment($submission->sid); ?>
-</blockquote>
-
-<?php
-
-/**
- * Helper function to get a submission comment.
- */
-function _emh_request_submission_get_comment($sid) {
-  $query = db_select('flagging', 'f');
-  $query->join('field_data_field_comment_answer', 'c', 'f.flagging_id = c.entity_id');
-  $query
-    ->condition('f.entity_type', 'webform_submission')
-    ->condition('f.fid', 16)
-    ->condition('f.entity_id', $sid)
-    ->fields('c', array('field_comment_answer_value'));
-  $result = $query->execute();
-  $row = $result->fetch();
-
-  return ($row->field_comment_answer_value);
-}
-?>
+<?php if (isset($comment_answer)) : ?>
+  <h3>Your feedback comment</h3>
+  <blockquote><?php print $comment_answer; ?></blockquote>
+<?php endif; ?>
