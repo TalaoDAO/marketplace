cd27619d660fc323f925cd8249b37a0a79ac12ee
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-27 18:10:34 +0100

    ADD english default content #1072

--- www/sites/all/modules/custom/emh_smartmobility/theme/block--emh-smartmobility--persona.tpl.php
+++ www/sites/all/modules/custom/emh_smartmobility/theme/block--emh-smartmobility--persona.tpl.php
@@ -1,8 +1,8 @@
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $title; ?>
-  <?php print t('Une initiative dédiée au redéploiement des salariés d\'Airbus dans le secteur de l\'aéronautique'); ?>
-  <?php print t('Vous êtes un partenaire'); ?>
-  <?php print t('Vous êtes salarié Airbus'); ?>
+  <?php print t('An initiative dedicated to the redeployment of Airbus employees in the aerospace sector'); ?>
+  <?php print t('Looking for talent'); ?>
+  <?php print t('I am an employee of Airbus'); ?>
 
 </section><!-- /.select-persona-wrapper -->
