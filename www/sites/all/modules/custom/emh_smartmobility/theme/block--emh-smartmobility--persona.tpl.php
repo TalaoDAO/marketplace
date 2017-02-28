904b969a7f50aa89ceff3452a34229072ff0ae29
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-02-28 07:14:14 +0100

    ADD: missing translations + CHG: links format

--- www/sites/all/modules/custom/emh_smartmobility/theme/block--emh-smartmobility--persona.tpl.php
+++ www/sites/all/modules/custom/emh_smartmobility/theme/block--emh-smartmobility--persona.tpl.php
@@ -1,7 +1,7 @@
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $title; ?>
-  <?php print t('An initiative dedicated to the redeployment of Airbus employees in the aerospace sector'); ?>
+  <?php echo sprintf(t('An initiative dedicated to the redeployment of Airbus employees %sin the aerospace sector'), '<br />'); ?>
   <?php print t('Looking for talent'); ?>
   <?php print t('I am an employee of Airbus'); ?>
 
