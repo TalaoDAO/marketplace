20633083b16848846dbc8e637261439cb2f8526e
Yoann Babel <yoann.babel@emindhub.com>
2017-03-27 12:00:57 +0200

    seurity update

--- www/sites/all/modules/contrib/views_send/views_send.js
+++ www/sites/all/modules/contrib/views_send/views_send.js
@@ -1,4 +1,11 @@
 (function ($) {
+  // Polyfill for jQuery less than 1.6.
+  if (typeof $.fn.prop != 'function') {
+    jQuery.fn.extend({
+      prop: jQuery.fn.attr
+    });
+  }
+
   Drupal.behaviors.viewsSend = {
     attach: function(context) {
       $('.views-send-selection-form', context).each(function() {
@@ -14,7 +21,7 @@
     // This is the "select all" checkbox in (each) table header.
     $('.views-send-table-select-all', form).click(function() {
       var table = $(this).closest('table')[0];
-      $('input[id^="edit-views-send"]:not(:disabled)', table).attr('checked', this.checked).change();
+      $('input[id^="edit-views-send"]:not(:disabled)', table).prop('checked', this.checked).change();
     });
   }
 
@@ -23,21 +30,21 @@
     $('.views-send-select-all-markup', form).show();
 
     $('.views-send-select-this-page', form).click(function() {
-      $('input[id^="edit-views-send"]', form).attr('checked', this.checked).change();
+      $('input[id^="edit-views-send"]', form).prop('checked', this.checked).change();
 
       // Toggle the "select all" checkbox in grouped tables (if any).
-      $('.views-send-table-select-all', form).attr('checked', this.checked).change();
+      $('.views-send-table-select-all', form).prop('checked', this.checked).change();
     });
 
     $('.views-send-select', form).click(function() {
       // If a checkbox was deselected, uncheck any "select all" checkboxes.
       if (!this.checked) {
-        $('.views-send-select-this-page', form).attr('checked', false).change();
+        $('.views-send-select-this-page', form).prop('checked', false).change();
 
         var table = $(this).closest('table')[0];
         if (table) {
           // Uncheck the "select all" checkbox in the table header.
-          $('.views-send-table-select-all', table).attr('checked', false).change();
+          $('.views-send-table-select-all', table).prop('checked', false).change();
         }
       }
     });
