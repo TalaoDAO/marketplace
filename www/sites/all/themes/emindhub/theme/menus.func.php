1a59672ddec0ca864c22afcca7c04849fd8d5f15
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-05-10 19:40:25 +0200

    ADD: complete theme #1139

--- www/sites/all/themes/emindhub/theme/menus.func.php
+++ www/sites/all/themes/emindhub/theme/menus.func.php
@@ -7,7 +7,7 @@ function emindhub_preprocess_menu_link(&$vars) {
   // TODO: se baser sur le chemin system ?
   switch ($element['#original_link']['mlid']) {
     // User menu > Account
-    case '7579':
+    case '7702':
       global $user;
       $account = user_load($user->uid);
       $firstName = '';
@@ -52,21 +52,21 @@ function emindhub_menu_link__user_menu(&$vars) {
  * Implements theme_menu_tree__MENU_NAME().
  */
 function emindhub_menu_tree__user_menu(&$variables) {
-  return '<ul class="menu nav navbar-nav navbar-right">' . $variables['tree'] . '</ul>';
+  return '<ul id="system-user-menu" class="menu nav navbar-nav navbar-right">' . $variables['tree'] . '</ul>';
 }
 
 /**
  * Implements theme_menu_tree__MENU_NAME().
  */
 function emindhub_menu_tree__menu_top(&$variables) {
-  return '<ul class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
+  return '<ul id="menu-menu-top" class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
 }
 
 /**
  * Implements theme_menu_tree__MENU_NAME().
  */
 function emindhub_menu_tree__menu_top_anonymous(&$variables) {
-  return '<ul class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
+  return '<ul id="menu-menu-top-anonymous" class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
 }
 
 /**
