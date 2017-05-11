59619b6fe435253bc562187cc748022c9bae1bcc
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-05-11 08:38:32 +0200

    FIX: mobile complete profile form

--- www/sites/all/themes/emindhub/theme/regions.func.php
+++ www/sites/all/themes/emindhub/theme/regions.func.php
@@ -1,7 +1,9 @@
 <?php
 
+/**
+ * Implements template_preprocess_region().
+ */
 function emindhub_preprocess_region(&$variables, $hook) {
-
   $region_id = $variables['region'];
   $classes = &$variables['classes_array'];
 
@@ -11,6 +13,7 @@ function emindhub_preprocess_region(&$variables, $hook) {
         $classes[] = 'col-md-9';
       endif;
       break;
+
     case 'header_right':
       if (drupal_is_front_page()) :
         $classes[] = 'col-md-6 col-md-offset-6';
@@ -18,18 +21,18 @@ function emindhub_preprocess_region(&$variables, $hook) {
         $classes[] = 'col-md-3';
       endif;
       break;
+
     case 'title':
     case 'top':
       $classes[] = 'row';
       break;
-    case 'highlighted':
-      $classes[] = 'row';
-      break;
+
     case 'sidebar_first':
-      $classes = array('region region-sidebar-first' ); // HACK for removing well class
+      // HACK for removing well class.
+      $classes = array('region region-sidebar-first');
       break;
+
     default:
       break;
   }
-
 }
