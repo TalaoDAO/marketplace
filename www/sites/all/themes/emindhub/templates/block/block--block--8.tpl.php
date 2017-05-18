806230edd67e7d5d05ec3357f584a0189868bdce
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-05-18 15:28:30 +0200

    CHG: hp ACTIONS #1152

--- www/sites/all/themes/emindhub/templates/block/block--block--8.tpl.php
+++ www/sites/all/themes/emindhub/templates/block/block--block--8.tpl.php
@@ -1,3 +1,4 @@
+<?php global $base_url; ?>
 <section id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
@@ -7,17 +8,32 @@
   <?php print render($title_suffix); ?>
 
   <div class="content">
-    <?php if (user_access('create request content')) : ?>
-      <?php echo sprintf(t('%sCreate a request%s'), '<a class="btn btn-client" href="' . url("node/add/request") . '"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;&nbsp;', '</a>'); ?>
+
+    <?php if (og_is_member('node', '2520', 'user') || og_is_member('node', '2564', 'user')) : ?>
+      <div class="col-md-3 highlight-circles">
+        <?php if (og_is_member('node', '2520', 'user')) : ?>
+          <a href="<?php print $base_url . '/ ' . url('node/2520'); ?>"><?php print emh_circles_get_circle_logo(node_load('2520')); ?></a>
+        <?php endif; ?>
+
+        <?php if (og_is_member('node', '2564', 'user')) : ?>
+          <a href="<?php print $base_url . '/ ' . url('node/2564'); ?>"><?php print emh_circles_get_circle_logo(node_load('2564')); ?></a>
+        <?php endif; ?>
+      </div>
     <?php endif; ?>
 
-    <?php if (user_access('invite experts')) : ?>
-      <a class="btn btn-flash icon-user" href="<?php print url('invitations'); ?>"><?php print t('Invite experts'); ?></a>
-    <?php endif; ?>
+    <div class="col-md-9">
+      <?php if ($logged_in) : ?>
+        <a class="btn btn-flash icon-community" href="<?php print url('circles'); ?>"><?php print t('Join circles'); ?></a>
+      <?php endif; ?>
 
-    <?php if (user_has_role(3) || user_has_role(4) || user_has_role(5) || user_has_role(6)) : ?>
-      <a class="btn btn-flash icon-community" href="<?php print url('circles'); ?>"><?php print t('Join circles'); ?></a>
-    <?php endif; ?>
+      <?php if (user_access('create request content')) : ?>
+        <?php echo sprintf(t('%sCreate a request%s'), '<a class="btn btn-client" href="' . url("node/add/request") . '"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;&nbsp;', '</a>'); ?>
+      <?php endif; ?>
+
+      <?php if (user_access('invite experts')) : ?>
+        <a class="btn btn-flash icon-user" href="<?php print url('invitations'); ?>"><?php print t('Invite experts'); ?></a>
+      <?php endif; ?>
+    </div>
 
   </span>
 
