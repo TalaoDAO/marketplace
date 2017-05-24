1d59c2212f3c7ead7a80ecb63a99fe65dd3eaa78
Laetitia Debruyne <pro@laetitiadebruyne.com>
2017-05-24 15:06:27 +0200

    FIX: remove affix for Safari browsers #1149

--- www/sites/all/themes/emindhub/js/theme/affix.js
+++ www/sites/all/themes/emindhub/js/theme/affix.js
@@ -1,13 +1,18 @@
-/*(function ($) {
+(function ($) {
     $(document).ready(function () {
+      /* Check if we are in safari */
+        var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+        if (isSafari) {
+            return;
+        }
+
         if ($('.js-affix').length) {
-          $('.js-affix').affix({
-              offset: {
-                  top: $('.view-query-list').offset().top,
-                  bottom: ($('.more-link').outerHeight(true) + $('#block-views-news-thread-publications-block').outerHeight(true) + $('footer').outerHeight(true)) + 40
-              }
-          });
+            $('.js-affix').affix({
+                offset: {
+                    top: $('.view-query-list').offset().top,
+                    bottom: ($('.more-link').outerHeight(true) + $('#block-views-news-thread-publications-block').outerHeight(true) + $('footer').outerHeight(true)) + 40
+                }
+            });
         }
     });
 })(jQuery);
-*/
