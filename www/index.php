506e914185b2487d71083de082878a80587f6e44
Vagrant Default User <vagrant@emh.box.local>
2017-06-16 15:59:29 +0200

    correction code + modif upload image linkedin

--- www/index.php
+++ www/index.php
@@ -1,9 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-
 /**
  * @file
  * The PHP page that serves all page requests on a Drupal installation.
