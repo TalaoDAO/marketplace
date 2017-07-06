58ed7c25f5c839bf8608c0f3b6376b7f186e1411
Yoann Babel <yoann.babel@emindhub.com>
2017-07-06 17:08:05 +0200

    Merge branch 'sm-videos' into 'dev1'
    #1191 tuto videos sur la HP SM
    
    See merge request !722

---
+++ www/sites/all/themes/smartmobility/theme-settings.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Alters the Smart Mobility theme settings form.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function smartmobility_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
+  }
+
+  $form['sm'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Smart Mobility settings'),
+    '#collapsible' => FALSE,
+    '#collapsed' => FALSE,
+    '#weight' => -50,
+  );
+
+  // Customers and Experts tutorial videos.
+  $form['sm']['video_customers'] = array(
+    '#type' => 'textfield',
+    '#title' => t('URL of the Customers video'),
+    '#default_value' => theme_get_setting('video_customers'),
+    '#description' => t('Specify pages by using their paths. Enter one path per line.'),
+  );
+  $form['sm']['video_experts'] = array(
+    '#type' => 'textfield',
+    '#title' => t('URL of the Experts video'),
+    '#default_value' => theme_get_setting('video_experts'),
+    '#description' => t('Specify pages by using their paths. Enter one path per line.'),
+  );
+}
