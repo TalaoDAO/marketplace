0b1840dd7d51722d71191e7cf4ab848b4f2a67ad
Yoann Babel <yoann.babel@emindhub.com>
2017-08-02 11:25:47 +0200

    Add anonymous paths

---
+++ visual_regression/backstop_scripts/anon_paths.js
@@ -0,0 +1,11 @@
+var pathConfig = {};
+
+pathConfig.array = ['/',
+'/contact',
+'/legal/document/terms-of-use',
+'/client/register',
+'/expert/register',
+'/how-it-works',
+'about-us']
+
+module.exports = pathConfig;
