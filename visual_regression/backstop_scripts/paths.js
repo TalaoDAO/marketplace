8e61f5588a979c7fc5c193622f7febb48608d4fb
Yoann Babel <yoann.babel@emindhub.com>
2017-08-03 11:29:22 +0200

    ADD: anon path supported in path.js now

--- visual_regression/backstop_scripts/paths.js
+++ visual_regression/backstop_scripts/paths.js
@@ -6,3 +6,16 @@ pathConfig.array = ['/',
 '/my/circles']
 
 module.exports = pathConfig;
+
+var anon_pathConfig = {};
+
+anon_pathConfig.array = ['/',
+'/contact',
+'/legal/document/terms-of-use',
+'/client/register',
+'/expert/register',
+'/how-it-works',
+'about-us']
+
+module.exports = anon_pathConfig;
+
