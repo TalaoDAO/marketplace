596ce82abca49291b26126418d51b9d0288d9db0
Benjamin Gagnaire <benjamin.gagnaire@emindhub.com>
2017-10-09 16:11:27 +0200

    add module node_authored_select

---
+++ www/sites/all/modules/contrib/node_authored_select/node_authored.api.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * API documentation for  Node "Authored by" select
+ */
+
+/**
+ * Change the select users query.
+ */
+function hook_node_authored_select_query_alter(&$query, $rid) {
+}
+
