34960c6b81c1b7c7f9f25406c6ccba57ebeec953
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-11-25 14:11:29 +0100

    lots of things in ethereum_user module + update web3 to 1.0 last beta

--- www/sites/all/modules/custom/ethereum/ethereum_user/contracts/ethereum_user_register_drupal/js/ethereum_user_register_drupal.js
+++ www/sites/all/modules/custom/ethereum/ethereum_user/contracts/ethereum_user_register_drupal/js/ethereum_user_register_drupal.js
@@ -9,32 +9,49 @@
       window.addEventListener('load', function () {
 
         // Checking if Web3.js has been injected by the browser (Mist/MetaMask).
-        // TODO: factor for general use.
+        // TODO: check that Mist also sets web3.eth.accounts[0]
         if (typeof web3 !== 'undefined') {
           window.web3 = new Web3(web3.currentProvider);
+          var user_address = web3.eth.accounts[0];
         }
         // Else fallback on a locally injected Web3.js.
         else {
-          window.web3 = new Web3(new Web3.providers.HttpProvider(Drupal.settings.ethereum_user.ethereum_user_register_drupal_fallback_node));
+          window.web3 = new Web3(new Web3.providers.HttpProvider(Drupal.settings.ethereum_user.fallback_node));
+          var user_address = Drupal.settings.ethereum_user.user.address;
         }
 
-        // Helper function to get a contract.
-        // TODO: factor for general use.
-        getContract = function (address, abiJSON) {
-          abi = JSON.parse(abiJSON);
-          contract_holder = web3.eth.contract(abi);
-          return contract_holder.at(address);
-        }
+        // Get contract object.
+        var contract_abi = JSON.parse(Drupal.settings.ethereum_user.contract.abi);
+        var contract_address = Drupal.settings.ethereum_user.contract.address;
+        var contract = new web3.eth.Contract(contract_abi, contract_address);
 
-        // Main code.
-        $('#ethereum_user_register_drupal_trigger').click(function () {
-          contract = getContract(Drupal.settings.ethereum_user.ethereum_user_register_drupal_deployed_contract_address, Drupal.settings.ethereum_user.ethereum_user_register_drupal_deployed_contract_abi);
-          hash = Drupal.settings.ethereum_user.ethereum_user_register_drupal_user_hash;
-          contract.newUser(hash, {from: web3.eth.accounts[0]});
-          address = contract.validateUserByHash(hash);
-          console.log(address);
-          // TODO: listen to transaction receipt and update #ethereum_user_register_drupal_trigger HTML.
-          // TODO: then, Ajax / Drupal service callback to assign the 'registered ethereum user' role to the user.
+        // User hash.
+        var user_hash = Drupal.settings.ethereum_user.user.hash;
+
+        // Check if the user already signed the contract.
+        contract.methods.validateUserByHash(user_hash).call({from: user_address}, function (error, result) {
+          if (error) {
+            console.log(error);
+          }
+          else if (result.toLowerCase() == user_address.toLowerCase()) {
+            $('#ethereum_user_registry_wait').toggle('fast');
+            $('#ethereum_user_registry_validated').toggle('fast');
+          }
+          else {
+            $('#ethereum_user_registry_wait').toggle('fast');
+            $('#ethereum_user_registry_sign').toggle('fast');
+            $('#ethereum_user_registry_sign a').click(function () {
+              $('#ethereum_user_registry_sign').html('<p>Your Ethereum wallet is waiting for you to sign this transaction.</p>');
+              contract.methods.newUser(user_hash).send({from: user_address}, function (error, transactionHash) {
+                if (error) {
+                  console.log(error);
+                }
+                else {
+                  $('#ethereum_user_registry_sign').html('<p>Please wait between 10 seconds and a few minutes for the transaction to be mined on the Ethereum network. You can reload this page at any time to see if the transaction is confirmed. Or you can <a href="https://etherscan.io/tx/' + transactionHash + '" target="_blank">see the transaction status in live</a>.</p>');
+                }
+              });
+            });
+          }
         });
       });
     }
