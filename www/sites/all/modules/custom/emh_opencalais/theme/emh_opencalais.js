1944def2d92b95d5951567d35f759587e97799ea
Benjamin Gagnaire <benjamin.gagnaire@emindhub.com>
2017-11-27 10:25:12 +0100

    maj js script to drupal #1216

--- www/sites/all/modules/custom/emh_opencalais/theme/emh_opencalais.js
+++ www/sites/all/modules/custom/emh_opencalais/theme/emh_opencalais.js
@@ -1,11 +1,17 @@
+/**
+ * @file emh_opencalais.js
+ * Show or hide button to suggest tags
+ */
 (function($) {
-  jQuery(document).ready(function () { 		
-    $("#edit-language").change(function () {		   
-      if($('#edit-language').val() == 'fr') {
-        $('.emh_opencalais_button_holder').hide();
-      } else {
-        $('.emh_opencalais_button_holder').show();
-      }
-    }).change();	
-  });
+  Drupal.behaviors.emhOpencalais = {
+    attach: function(context, settings) {
+      $("#edit-language").change(function () {		   
+        if($('#edit-language').val() == 'fr') {
+          $('.emh_opencalais_button_holder').hide();
+        } else {
+          $('.emh_opencalais_button_holder').show();
+        }
+      }).change();
+    }
+  }
 })(jQuery);
