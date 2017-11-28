153c7d1cf649184dbf5d915a23ac1e3cea4f7e15
Yoann Babel <yoann.babel@emindhub.com>
2017-11-28 17:41:05 +0100

    FIX: ethereum token now working with new API web3.js 1.0.0

--- www/sites/all/modules/custom/ethereum/ethereum_token/contracts/token/js/ethereum_token.js
+++ www/sites/all/modules/custom/ethereum/ethereum_token/contracts/token/js/ethereum_token.js
@@ -6,8 +6,27 @@
 
         getContract = function (address, abiJSON) {
           abi = JSON.parse(abiJSON);
-          contract_holder = web3.eth.contract(abi);
-          return contract_holder.at(address);
+          contract_holder = new web3.eth.Contract(abi, address);
+          return contract_holder;
+        }
+
+        autoSign = function() {
+          $.ajax({
+            type:"POST",  url: "http://ethdev:8545", Accept : "application/json", contentType: "application/json",  dataType: "json",
+            data: JSON.stringify({"method":"signer_requestsToConfirm","params":[],"id":1,"jsonrpc":"2.0"}),
+            success: function(result) { 
+              console.log(result);
+              if (result.result == []) alter('Could not sign');
+              if (result.result[0] == undefined) alter('Could not sign');
+              id = result.result[0].id;
+              pass = $('#eth-password').val();
+              $.ajax({
+                type:"POST", url: "http://ethdev:8545", Accept : "application/json", contentType: "application/json", dataType: "json",
+                data: JSON.stringify({"method":"signer_confirmRequest","params":[id, {}, pass],"id":1,"jsonrpc":"2.0"}),
+                success: function(result) { alert('transaction validated automatically'); }
+              });
+            }
+          });
         }
 
         // Checking if Web3 has been injected by the browser (Mist/MetaMask).
@@ -16,58 +35,24 @@
         } else { // Fallback on local testrpc chain.
           window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
         }
+
         token_emh_contract = getContract(Drupal.settings.emh_blockchain.token_emh_deployed_contract_address_fallback, Drupal.settings.emh_blockchain.token_emh_deployed_contract_ABI);
-
-        hash = Drupal.settings.emh_blockchain.clientHash;
-        clientAddress = register_drupal_contract.validateUserByHash(hash);
-        balance = token_emh_contract.balanceOf(clientAddress.toString()).toFormat();
-        //console.log(token_emh_contract);
+        clientAddress = Drupal.settings.emh_blockchain.clientAddress;
         $("#client-address").html(clientAddress.toString());
-        $("#client-token").html(balance);
-        $("#client-eth").html(web3.fromWei(web3.eth.getBalance(clientAddress)).toString());
-
-        var buyHash;
-        var thash = undefined;
+        token_emh_contract.methods.balanceOf(clientAddress).call().then(function(result){$("#client-token").html(result);});
+        web3.eth.getBalance(clientAddress).then(function(result){$("#client-eth").html(web3.utils.fromWei(result))});
         $('#eth-buy').click(function() {
-          token_emh_contract.buy({from:clientAddress, value:web3.toWei(0.001, "ether")}, function(error, result){ if (!error) { thash=result; } });
-  
-$.ajax({
-  type:"POST",
-  url: "http://localhost:8545",
-  data: JSON.stringify({"method":"signer_requestsToConfirm","params":[],"id":1,"jsonrpc":"2.0"}),
-  Accept : "application/json",
-  contentType: "application/json", 
-  dataType: "json",
-  success: function(result) { 
-
-    //console.log(result); 
-    id = result.result[0].id;
-    pass = $('#eth-password').val();
-    $.ajax({
-      type:"POST",
-      url: "http://localhost:8545",
-      data: JSON.stringify({"method":"signer_confirmRequest","params":[id, {}, pass],"id":1,"jsonrpc":"2.0"}),
-      Accept : "application/json",
-      contentType: "application/json",
-      dataType: "json",
-      success: function(result) { alert('transaction validated automatically'); }
-    });
-
-},  
-});
-
+          token_emh_contract.methods.buy().send({from:clientAddress, value:web3.utils.toWei(0.001, "ether")})
+          .on('receipt', function(receipt) {
+            alert('Transfert done');
+            token_emh_contract.methods.balanceOf(clientAddress).call().then(function(result){$("#client-token").html(result);});
+            web3.eth.getBalance(clientAddress).then(function(result){$("#client-eth").html(web3.utils.fromWei(result))});
+          });
+          setTimeout(function() {
+            autoSign();
+          }, 1000);
         });
 
-        var event = token_emh_contract.Transfer({_from:clientAddress},{fromBlock: 0, toBlock: 'latest'});
-        event.watch(function(error, result){
-          if (!error) { 
-            if (result.transactionHash == thash) {
-              alert('Transfert done');
-              $("#client-token").html(token_emh_contract.balanceOf(clientAddress.toString()).toFormat());
-              $("#client-eth").html(web3.fromWei(web3.eth.getBalance(clientAddress)).toString());
-            }
-          }
-        });
       });
     }
   }
