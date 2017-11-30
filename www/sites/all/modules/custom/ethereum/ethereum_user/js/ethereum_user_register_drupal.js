266978fe209e98b8e8517294faf1c6891fa22835
Guillaume Duveau <guillaume.duveau@gmail.com>
2017-11-30 18:54:10 +0100

    ethereum_user: use events tup update signature status

--- www/sites/all/modules/custom/ethereum/ethereum_user/js/ethereum_user_register_drupal.js
+++ www/sites/all/modules/custom/ethereum/ethereum_user/js/ethereum_user_register_drupal.js
@@ -9,7 +9,7 @@
       window.addEventListener('load', function () {
 
         // Checking if Web3.js has been injected by the browser (Mist/MetaMask).
-        // NOTE: MetaMask does not support Web3 1.0.0 yet, this won't work, check the metamask branch.
+        // NOTE: MetaMask does not support Web3 1.0.0 yet, this won't work, check the metamask branch. It should (maybe) work when Metamask updates to 1.0.0.
         if (typeof web3 !== 'undefined') {
           window.web3 = new Web3(web3.currentProvider);
           var accounts = web3.eth.getAccounts(function (error, accounts) {
@@ -42,7 +42,6 @@
             console.log(error);
           }
           else if (result.toLowerCase() == user_address) {
-            console.log(user_address);
             $('#ethereum_user_registry_wait').toggle('fast');
             $('#ethereum_user_registry_validated').toggle('fast');
           }
@@ -51,13 +50,25 @@
             $('#ethereum_user_registry_sign').toggle('fast');
             $('#ethereum_user_registry_sign a').click(function () {
               $('#ethereum_user_registry_sign').html('<p>Your Ethereum wallet is waiting for you to sign this transaction.</p>');
-              contract.methods.newUser(user_hash).send({from: user_address}, function (error, transactionHash) {
-                if (error) {
-                  console.log(error);
+
+              // TODO: handle the error if the user rejects the transaction.
+              contract.methods.newUser(user_hash).send({from: user_address})
+              .on('transactionHash', function (transactionHash) {
+                $('#ethereum_user_registry_sign').html('<p>Please wait between 10 seconds and a few minutes for the transaction to be mined on the Ethereum network. You can reload this page at any time to see if the transaction is confirmed. Or you can <a href="https://etherscan.io/tx/' + transactionHash + '" target="_blank">see the transaction status in live</a>.</p>');
+              })
+              .on('error', function (error) {
+                // 0 is "success error" in RegisterDrupal.sol
+                if (error != 0) {
+                  console.error;
                 }
-                else {
-                  $('#ethereum_user_registry_sign').html('<p>Please wait between 10 seconds and a few minutes for the transaction to be mined on the Ethereum network. You can reload this page at any time to see if the transaction is confirmed. Or you can <a href="https://etherscan.io/tx/' + transactionHash + '" target="_blank">see the transaction status in live</a>.</p>');
-                }
+              })
+              .on('confirmation', function (confirmationNumber, receipt) {
+                // Should be 0 because there's no block after this transaction yet.
+                console.log('Number of confirmation blocks: ' + confirmationNumber);
+              })
+              .then(function (receipt) {
+                // Fires once the receipt is mined.
+                $('#ethereum_user_registry_sign').html('<p>Congratulations! Ethereum network just confirmed your signature of our user registry. You can now use Ethereum features with this site.');
               });
             });
           }
