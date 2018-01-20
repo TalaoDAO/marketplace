0e99fb677331131aba8d985fac4e6b517acc4fad
Emmanuel Fourquet <emmanuel.fourquet@hotmail.com>
2018-01-20 11:37:10 +0000

    .less and hero tpl

--- www/sites/all/themes/emindhub/js/theme/emh-modules/emindhub_emh_homepage.js
+++ www/sites/all/themes/emindhub/js/theme/emh-modules/emindhub_emh_homepage.js
@@ -7,12 +7,24 @@
   Drupal.behaviors.emindhub_emh_homepage = {
     attach: function (context, settings) {
 
+
+
+
+
+
       // Normal JS in here. Use $() instead of jQuery(), like:
       $('.emh-popup-closed').hide();
 
       // Sliders.
       $('.slick-how-need').slick();
       $('.slick-how-have').slick();
+
+
+
+
+
+
+
 
     }
   }
