9658ee6999f1e26f82adc2af8d2899e332afefbb
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-01-20 01:21:06 +0000

    theming

---
+++ www/sites/all/themes/emindhub/templates/modules/custom/emh_homepage/config-pages--homepage.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Template.
+ *
+ * $content => access all fields values.
+ * render($content) => renders all fields.
+ * $content['field_hp_hero_title'] => access all hp hero title fields.
+ * render($content['field_hp_hero_title']) => renders the hp hero title fields.
+ */
+?>
+<?php print render($content); ?>
