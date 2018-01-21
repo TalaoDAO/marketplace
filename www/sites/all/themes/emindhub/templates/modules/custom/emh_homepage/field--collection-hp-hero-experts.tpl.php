262c2bbcaa562701c5e582df890b63ec39fa9c0f
Guillaume Duveau <guillaume.duveau@gmail.com>
2018-01-21 16:04:42 +0000

    simplufy tempates

--- www/sites/all/themes/emindhub/templates/modules/custom/emh_homepage/field--collection-hp-hero-experts.tpl.php
+++ www/sites/all/themes/emindhub/templates/modules/custom/emh_homepage/field--collection-hp-hero-experts.tpl.php
@@ -1,12 +1,5 @@
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
-  <?php endif; ?>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?> hero-expert-<?php print $delta; ?>"<?php print $item_attributes[$delta]; ?>>
-        <?php print render($item); ?>
-      </div>
-    <?php endforeach; ?>
+<?php foreach ($items as $delta => $item): ?>
+  <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?> hero-expert hero-expert-<?php print $delta; ?>"<?php print $item_attributes[$delta]; ?>>
+    <?php print render($item); ?>
   </div>
-</div>
+<?php endforeach; ?>
--- www/sites/all/themes/emindhub/templates/modules/custom/emh_homepage/field--field-slide--field-hp-solutions-slides-blue.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($items as $delta => $item): ?>
-  <div class="col-xs-12 col-sm-4">
-    <?php print render($item); ?>
-  </div>
-<?php endforeach; ?>
