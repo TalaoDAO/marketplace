fb9d7d7faab8221a4f36cbc3f96a2ceb686c0d79
benjamin.gagnaire <be.gagnaire@gmail.com>
2018-01-24 03:13:03 +0100

    update form and behat #1221 #1218 #1222

---
+++ www/sites/all/modules/custom/emh_virality/theme/emh_virality.js
@@ -0,0 +1,14 @@
+/**
+ * @file emh_virality.js
+ * Show or hide button to suggest tags.
+ */
+(function($) {
+    Drupal.behaviors.emhVirality = {
+        attach: function(context, settings) {
+           // alert($('.sponsorship-button-remove'));
+            /*$('.sponsorship-button-remove').click(function () {
+                alert($('.sponsorship-button-remove'));
+            });*/
+        }
+    }
+})(jQuery);
